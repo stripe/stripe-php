f315932a726b28443579fb5014b12cc3dc8f874a
Sheena Pakanati <spakanati@gmail.com>
2011-07-27 12:54:41 -0700

    Plan creation

--- lib/Stripe.php
+++ lib/Stripe.php
@@ -60,3 +60,4 @@ require_once(dirname(__FILE__) . '/Stripe/Charge.php');
 require_once(dirname(__FILE__) . '/Stripe/Customer.php');
 require_once(dirname(__FILE__) . '/Stripe/Invoice.php');
 require_once(dirname(__FILE__) . '/Stripe/InvoiceItem.php');
+require_once(dirname(__FILE__) . '/Stripe/Plan.php');
