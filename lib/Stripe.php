282fa8a2e43f9c7f4e48ccf1f33f0dfbcd4e4602
Greg Brockman <gdb@gregbrockman.com>
2011-09-15 09:42:29 -0700

    Bump version

--- lib/Stripe.php
+++ lib/Stripe.php
@@ -16,7 +16,7 @@ abstract class Stripe
   public static $apiKey;
   public static $apiBase = 'https://api.stripe.com/v1';
   public static $verifySslCerts = true;
-  const VERSION = '1.5.10';
+  const VERSION = '1.5.11';
 
   public static function getApiKey()
   {
