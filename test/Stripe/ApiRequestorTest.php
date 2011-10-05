f2964bd530c9dc61d2f76d641707bab23ca1aecb
Greg Brockman <gdb@gregbrockman.com>
2011-10-05 13:25:09 -0700

    Add test case for arrays with null values

--- test/Stripe/ApiRequestorTest.php
+++ test/Stripe/ApiRequestorTest.php
@@ -7,5 +7,9 @@ class Stripe_ApiRequestorTest extends UnitTestCase
     $a = array('my' => 'value', 'that' => array('your' => 'example'), 'bar' => 1, 'baz' => null);
     $enc = Stripe_APIRequestor::encode($a);
     $this->assertEqual($enc, 'my=value&that%5Byour%5D=example&bar=1');
+
+    $a = array('that' => array('your' => 'example', 'foo' => null));
+    $enc = Stripe_APIRequestor::encode($a);
+    $this->assertEqual($enc, 'that%5Byour%5D=example');
   }
 }
