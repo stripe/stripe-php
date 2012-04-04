3413be801da0e4b7e9d56ed6a8c4dab528bac6b8
Patrick Collison <patrick@collison.ie>
2012-04-03 18:41:28 -0700

    Bump version

--- lib/Stripe.php
+++ lib/Stripe.php
@@ -16,7 +16,7 @@ abstract class Stripe
   public static $apiKey;
   public static $apiBase = 'https://api.stripe.com/v1';
   public static $verifySslCerts = true;
-  const VERSION = '1.6.2';
+  const VERSION = '1.6.3';
 
   public static function getApiKey()
   {
