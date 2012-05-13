b7fbf0cf66004449c0576c37fea61d5caed5c307
Evan Broder <evan@stripe.com>
2012-05-13 15:41:43 -0700

    Use a random string in coupon tests to make simultaneous runs safer

--- test/Stripe/CouponTest.php
+++ test/Stripe/CouponTest.php
@@ -5,11 +5,12 @@ class Stripe_CouponTest extends UnitTestCase
   public function testCreate()
   {
     authorizeFromEnv();
+    $id = 'test_coupon-' . self::randomString();
     $c = Stripe_Coupon::create(array('percent_off' => 25,
 				    'duration' => 'repeating',
 				    'duration_in_months' => 5,
-				    'id' => 'test_coupon'));
-    $this->assertEqual('test_coupon', $c->id);
+				    'id' => $id));
+    $this->assertEqual($id, $c->id);
     $this->assertEqual(25, $c->percent_off);
   }
 
