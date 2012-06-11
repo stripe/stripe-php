9c647e9a339281eca0f79fd730b54e1185014148
Jakub Vrana <jakub@vrana.cz>
2012-06-11 16:13:31 -0700

    Set default value to optional parameter
    Not passed in ApiResource::instanceUrl()

--- lib/Stripe/InvalidRequestError.php
+++ lib/Stripe/InvalidRequestError.php
@@ -2,7 +2,7 @@
 
 class Stripe_InvalidRequestError extends Stripe_Error
 {
-  public function __construct($message, $param, $http_status=null, $http_body=null, $json_body=null)
+  public function __construct($message, $param=null, $http_status=null, $http_body=null, $json_body=null)
   {
     parent::__construct($message, $http_status, $http_body, $json_body);
     $this->param = $param;
