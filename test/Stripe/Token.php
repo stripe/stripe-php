10f72b067cb2dfe972bfca4215904bf0016d7004
Ross Boucher <rboucher@gmail.com>
2012-10-30 11:51:51 -0700

    Fix test failures

--- test/Stripe/Token.php
+++ test/Stripe/Token.php
@@ -4,8 +4,8 @@ class Stripe_TokenTest extends UnitTestCase
 {
   public function testUrls()
   {
-    $this->assertEqual(Stripe_Token::classUrl('Stripe_Token'), '/tokens');
+    $this->assertEqual(Stripe_Token::classUrl('Stripe_Token'), '/v1/tokens');
     $token = new Stripe_Token('abcd/efgh');
-    $this->assertEqual($token->instanceUrl(), '/tokens/abcd%2Fefgh');
+    $this->assertEqual($token->instanceUrl(), '/v1/tokens/abcd%2Fefgh');
   }
 }
