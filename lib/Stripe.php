b70eb3e1c25f7ff9f218af674defe3d3b578ca29
Scott Langendyk <scott@langendyk.com>
2012-11-07 14:48:46 -0500

    Move the Stripe class declaration to it's own file.

--- lib/Stripe.php
+++ lib/Stripe.php
@@ -10,33 +10,8 @@ if (!function_exists('json_decode')) {
   throw new Exception('Stripe needs the JSON PHP extension.');
 }
 
-
-abstract class Stripe
-{
-  public static $apiKey;
-  public static $apiBase = 'https://api.stripe.com';
-  public static $verifySslCerts = true;
-  const VERSION = '1.7.8';
-
-  public static function getApiKey()
-  {
-    return self::$apiKey;
-  }
-
-  public static function setApiKey($apiKey)
-  {
-    self::$apiKey = $apiKey;
-  }
-
-  public static function getVerifySslCerts() {
-    return self::$verifySslCerts;
-  }
-
-  public static function setVerifySslCerts($verify) {
-    self::$verifySslCerts = $verify;
-  }
-}
-
+// Stripe singleton
+require(dirname(__FILE__) . '/Stripe/Stripe.php');
 
 // Utilities
 require(dirname(__FILE__) . '/Stripe/Util.php');
