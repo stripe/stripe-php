8a620dee268ce1f1d558b039456affa8a8f8ca18
John Piasetzki <john@piasetzki.name>
2013-07-07 15:09:11 -0400

    Add an append option and refactor methods to use ApiResource functions

--- lib/Stripe/Invoice.php
+++ lib/Stripe/Invoice.php
@@ -28,10 +28,8 @@ class Stripe_Invoice extends Stripe_ApiResource
 
   public static function upcoming($params=null, $apiKey=null)
   {
-    $requestor = new Stripe_ApiRequestor($apiKey);
-    $url = self::classUrl(get_class()) . '/upcoming';
-    list($response, $apiKey) = $requestor->request('get', $url, $params);
-    return Stripe_Util::convertToStripeObject($response, $apiKey);
+    $class = get_class();
+    return self::_scopedAll($class, $params, $apiKey, '/upcoming');
   }
 
   public function save()
@@ -42,10 +40,7 @@ class Stripe_Invoice extends Stripe_ApiResource
 
   public function pay()
   {
-    $requestor = new Stripe_ApiRequestor($this->_apiKey);
-    $url = $this->instanceUrl() . '/pay';
-    list($response, $apiKey) = $requestor->request('post', $url);
-    $this->refreshFrom($response, $apiKey);
-    return $this;
+    $class = get_class();
+    return self::_scopedCreate($class, $params, $this->_apiKey, '/pay');
   }
 }
