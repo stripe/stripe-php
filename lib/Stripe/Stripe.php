6d5937f60b1c3063bca358c79e8232c079aa693d
Michelle Bu <michelle@stripe.com>
2013-10-16 17:23:45 -0700

    Add version #

--- lib/Stripe/Stripe.php
+++ lib/Stripe/Stripe.php
@@ -6,7 +6,7 @@ abstract class Stripe
   public static $apiBase = 'https://api.stripe.com';
   public static $apiVersion = null;
   public static $verifySslCerts = true;
-  const VERSION = '1.8.3';
+  const VERSION = '1.9.0';
 
   public static function getApiKey()
   {
