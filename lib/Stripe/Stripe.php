375227f845c99b2550a5c26d2a4911313cadb97c
Jim Danz <jim@stripe.com>
2014-01-29 17:00:33 -0800

    Bump version to 1.11.0

--- lib/Stripe/Stripe.php
+++ lib/Stripe/Stripe.php
@@ -6,7 +6,7 @@ abstract class Stripe
   public static $apiBase = 'https://api.stripe.com';
   public static $apiVersion = null;
   public static $verifySslCerts = true;
-  const VERSION = '1.10.1';
+  const VERSION = '1.11.0';
 
   public static function getApiKey()
   {
