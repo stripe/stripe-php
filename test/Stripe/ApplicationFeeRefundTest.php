fffb73d40aa6e51e04256b9c5a972417774670c3
Brian Krausz <bkrausz@stripe.com>
2014-07-29 21:48:58 -0700

    Fix lint error in ApplicationFeeRefundTest

--- test/Stripe/ApplicationFeeRefundTest.php
+++ test/Stripe/ApplicationFeeRefundTest.php
@@ -8,7 +8,9 @@ class Stripe_ApplicationFeeRefundTest extends StripeTestCase
     $refund->id = 'refund_id';
     $refund->fee = 'fee_id';
 
-    $this->assertEqual($refund->instanceUrl(),
-      '/v1/application_fees/fee_id/refunds/refund_id');
+    $this->assertEqual(
+        $refund->instanceUrl(),
+        '/v1/application_fees/fee_id/refunds/refund_id'
+    );
   }
 }
