72cd0e9539442742537603f0ff09fce89c7ed46d
Chad Gray <chadwickgray@gmail.com>
2015-01-10 17:47:45 +0000

    Add PHPUnit test for Stripe_Token

---
+++ tests/Stripe/TokenTest.php
@@ -0,0 +1,11 @@
+<?php
+
+class Stripe_TokenTest extends PHPUnit_Framework_TestCase
+{
+  public function testUrls()
+  {
+    $this->assertSame(Stripe_Token::classUrl('Stripe_Token'), '/v1/tokens');
+    $token = new Stripe_Token('abcd/efgh');
+    $this->assertSame($token->instanceUrl(), '/v1/tokens/abcd%2Fefgh');
+  }
+}
