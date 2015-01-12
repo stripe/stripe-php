5077fbcfe681c5239f65661c5e894fa2002b5cd1
wangjohn <john.jian.wang@gmail.com>
2015-01-12 13:51:33 -0800

    Add ability to list file uploads.

--- lib/Stripe/FileUpload.php
+++ lib/Stripe/FileUpload.php
@@ -35,4 +35,16 @@ class Stripe_FileUpload extends Stripe_ApiResource
     $class = get_class();
     return self::_scopedCreate($class, $params, $apiKey);
   }
+
+  /**
+   * @param array|null $params
+   * @param string|null $apiKey
+   *
+   * @return array An array of Stripe_FileUploads
+   */
+  public static function all($params=null, $apiKey=null)
+  {
+    $class = get_class();
+    return self::_scopedAll($class, $params, $apiKey);
+  }
 }
