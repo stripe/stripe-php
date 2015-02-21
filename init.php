52b803abe72606843bb0937bf29178740d686176
Brian Krausz <bkrausz@stripe.com>
2015-02-21 01:37:18 -0800

    Add init file

---
+++ init.php
@@ -0,0 +1,49 @@
+<?php
+
+// Stripe singleton
+require(dirname(__FILE__) . '/lib/Stripe.php');
+
+// Utilities
+require(dirname(__FILE__) . '/lib/Util/RequestOptions.php');
+require(dirname(__FILE__) . '/lib/Util/Set.php');
+require(dirname(__FILE__) . '/lib/Util/Util.php');
+
+// Errors
+require(dirname(__FILE__) . '/lib/Error/Base.php');
+require(dirname(__FILE__) . '/lib/Error/Api.php');
+require(dirname(__FILE__) . '/lib/Error/ApiConnection.php');
+require(dirname(__FILE__) . '/lib/Error/Authentication.php');
+require(dirname(__FILE__) . '/lib/Error/Card.php');
+require(dirname(__FILE__) . '/lib/Error/InvalidRequest.php');
+require(dirname(__FILE__) . '/lib/Error/RateLimit.php');
+
+// Plumbing
+require(dirname(__FILE__) . '/lib/Object.php');
+require(dirname(__FILE__) . '/lib/ApiRequestor.php');
+require(dirname(__FILE__) . '/lib/ApiResource.php');
+require(dirname(__FILE__) . '/lib/SingletonApiResource.php');
+require(dirname(__FILE__) . '/lib/AttachedObject.php');
+
+// Stripe API Resources
+require(dirname(__FILE__) . '/lib/Account.php');
+require(dirname(__FILE__) . '/lib/ApplicationFee.php');
+require(dirname(__FILE__) . '/lib/ApplicationFeeRefund.php');
+require(dirname(__FILE__) . '/lib/Balance.php');
+require(dirname(__FILE__) . '/lib/BalanceTransaction.php');
+require(dirname(__FILE__) . '/lib/BitcoinReceiver.php');
+require(dirname(__FILE__) . '/lib/BitcoinTransaction.php');
+require(dirname(__FILE__) . '/lib/Card.php');
+require(dirname(__FILE__) . '/lib/Charge.php');
+require(dirname(__FILE__) . '/lib/Collection.php');
+require(dirname(__FILE__) . '/lib/Coupon.php');
+require(dirname(__FILE__) . '/lib/Customer.php');
+require(dirname(__FILE__) . '/lib/Event.php');
+require(dirname(__FILE__) . '/lib/FileUpload.php');
+require(dirname(__FILE__) . '/lib/Invoice.php');
+require(dirname(__FILE__) . '/lib/InvoiceItem.php');
+require(dirname(__FILE__) . '/lib/Plan.php');
+require(dirname(__FILE__) . '/lib/Recipient.php');
+require(dirname(__FILE__) . '/lib/Refund.php');
+require(dirname(__FILE__) . '/lib/Subscription.php');
+require(dirname(__FILE__) . '/lib/Token.php');
+require(dirname(__FILE__) . '/lib/Transfer.php');
