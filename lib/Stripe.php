e0767fd47d01d3cd4bb1bffd5759d1cb76303f18
Kyle Conroy <kjc@stripe.com>
2016-05-04 14:19:42 -0700

    Bump version to 3.22.0

--- lib/Stripe.php
+++ lib/Stripe.php
@@ -22,7 +22,7 @@ class Stripe
     // @var boolean Defaults to true.
     public static $verifySslCerts = true;
 
-    const VERSION = '3.12.1';
+    const VERSION = '3.22.0';
 
     /**
      * @return string The API key used for requests.
