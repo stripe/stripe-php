738ad32ccd620eea6d8c63d4724a39f8195708a7
Jacqueline <jxu703@gmail.com>
2016-05-04 14:18:39 -0700

    allow retrieval, listing, and creation of subs, change URL to v1/subs (#244)

--- tests/SubscriptionTest.php
+++ tests/SubscriptionTest.php
@@ -5,7 +5,7 @@ namespace Stripe;
 class SubscriptionTest extends TestCase
 {
 
-    public function testCreateUpdateCancel()
+    public function testCustomerCreateUpdateListCancel()
     {
         $planID = 'gold-' . self::generateRandomString(20);
         self::retrieveOrCreatePlan($planID);
@@ -25,6 +25,9 @@ class SubscriptionTest extends TestCase
         $this->assertSame($sub->plan->id, $planID);
         $this->assertSame($sub->quantity, 2);
 
+        $subs = $customer->subscriptions->all(array('limit'=>3));
+        $this->assertSame(get_class($subs->data[0]), 'Stripe\Subscription');
+
         $sub->cancel(array('at_period_end' => true));
 
         $sub = $customer->subscriptions->retrieve($sub->id);
@@ -32,6 +35,36 @@ class SubscriptionTest extends TestCase
         // @codingStandardsIgnoreStart
         $this->assertTrue($sub->cancel_at_period_end);
         // @codingStandardsIgnoreEnd
+    }
+
+    public function testCreateUpdateListCancel()
+    {
+        $planID = 'gold-' . self::generateRandomString(20);
+        self::retrieveOrCreatePlan($planID);
+
+        $customer = self::createTestCustomer();
+
+        $sub = Subscription::create(array('plan' => $planID, 'customer' => $customer->id));
+
+        $this->assertSame($sub->status, 'active');
+        $this->assertSame($sub->plan->id, $planID);
+
+        $sub->quantity = 2;
+        $sub->save();
+
+        $sub = Subscription::retrieve($sub->id);
+        $this->assertSame($sub->status, 'active');
+        $this->assertSame($sub->plan->id, $planID);
+        $this->assertSame($sub->quantity, 2);
+
+        $subs = Subscription::all(array('customer'=>$customer->id, 'plan'=>$planID, 'limit'=>3));
+        $this->assertSame(get_class($subs->data[0]), 'Stripe\Subscription');
+
+        $sub->cancel(array('at_period_end' => true));
+
+        $sub = Subscription::retrieve($sub->id);
+        $this->assertSame($sub->status, 'active');
+        $this->assertTrue($sub->cancel_at_period_end);
     }
 
     public function testDeleteDiscount()
