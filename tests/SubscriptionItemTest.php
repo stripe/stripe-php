b5717401b6f3d74182896b2803fce6c333e4def9
Kyle Conroy <kjc@stripe.com>
2016-09-23 10:49:37 -0700

    Update tests to use real API

--- tests/SubscriptionItemTest.php
+++ tests/SubscriptionItemTest.php
@@ -21,18 +21,18 @@ class SubscriptionItemTest extends TestCase
         $subItem->subscription = $sub->id;
         $subItem->save();
 
-        $subItem = SubscriptionItem::retrieve(array('id' => $subItem->id, 'subscription' => $sub->id));
+        $subItem = SubscriptionItem::retrieve($subItem->id);
         $this->assertSame($subItem->quantity, 2);
 
         // Update the quantity parameter one more time
-        $subItem = SubscriptionItem::update($subItem->id, array('quantity' => 3, 'subscription' => $sub->id));
+        $subItem = SubscriptionItem::update($subItem->id, array('quantity' => 3));
         $this->assertSame($subItem->quantity, 3);
 
         $subItems = SubscriptionItem::all(array('subscription'=>$sub->id, 'limit'=>3));
         $this->assertSame(get_class($subItems->data[0]), 'Stripe\SubscriptionItem');
         $this->assertSame(2, count($subItems->data));
 
-        $subItem->delete(array('subscription' => $sub->id));
+        $subItem->delete();
         $this->assertTrue($subItem->deleted);
     }
 }
