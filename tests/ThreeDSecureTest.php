c10c6483db9283fd8867e09f2fa93474630c1d0e
Olivier Bellone <ob@stripe.com>
2016-11-21 19:15:36 +0100

    Added 3DS retrieve endpoint

--- tests/ThreeDSecureTest.php
+++ tests/ThreeDSecureTest.php
@@ -4,6 +4,21 @@ namespace Stripe;
 
 class ThreeDSecureTest extends TestCase
 {
+    public function testRetrieve()
+    {
+        $this->mockRequest(
+            'GET',
+            '/v1/3d_secure/tdsrc_test',
+            array(),
+            array(
+                'id' => 'tdsrc_test',
+                'object' => 'three_d_secure'
+            )
+        );
+        $three_d_secure = ThreeDSecure::retrieve('tdsrc_test');
+        $this->assertSame($three_d_secure->id, 'tdsrc_test');
+    }
+
     public function testCreate()
     {
         $this->mockRequest(
