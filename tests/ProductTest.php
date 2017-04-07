4bb8837aa658f8b2c5ccd98ccf6ff6ddd86014b3
Remi Jannel <remi@stripe.com>
2017-04-07 16:31:20 -0400

    Remove raw PANS from our Test suite.
    Only one is left for the dispute tests because there isn't a token ready
    to use just yet for this test card number.

--- tests/ProductTest.php
+++ tests/ProductTest.php
@@ -142,14 +142,7 @@ class ProductSKUOrderTest extends TestCase
         $stripeOrder = Order::retrieve($order->id);
         $this->assertSame($order->metadata->foo, "bar");
 
-        $order->pay(array(
-            'source' => array(
-                'object' => 'card',
-                'number' => '4242424242424242',
-                'exp_month' => '05',
-                'exp_year' => '2017'
-            ),
-        ));
+        $order->pay(array('source' => 'tok_visa'));
         $this->assertSame($order->status, 'paid');
 
         $orderReturn = $order->returnOrder();
