400655367fe8b9006d1b654a3568086c7d7f59e6
Olivier Bellone <ob@stripe.com>
2017-10-24 18:44:35 +0200

    Static methods for accessing nested resources

--- tests/ApplicationFeeTest.php
+++ tests/ApplicationFeeTest.php
@@ -19,4 +19,75 @@ class ApplicationFeeTest extends TestCase
         $d = ApplicationFee::all();
         $this->assertSame($d->url, '/v1/application_fees');
     }
+
+    public function testStaticCreateRefund()
+    {
+        $this->mockRequest(
+            'POST',
+            '/v1/application_fees/fee_123/refunds',
+            array(),
+            array('id' => 'fr_123', 'object' => 'fee_refund')
+        );
+
+        $feeRefund = ApplicationFee::createRefund(
+            'fee_123'
+        );
+
+        $this->assertSame('fr_123', $feeRefund->id);
+        $this->assertSame('fee_refund', $feeRefund->object);
+    }
+
+    public function testStaticRetrieveRefund()
+    {
+        $this->mockRequest(
+            'GET',
+            '/v1/application_fees/fee_123/refunds/fr_123',
+            array(),
+            array('id' => 'fr_123', 'object' => 'fee_refund')
+        );
+
+        $feeRefund = ApplicationFee::retrieveRefund(
+            'fee_123',
+            'fr_123'
+        );
+
+        $this->assertSame('fr_123', $feeRefund->id);
+        $this->assertSame('fee_refund', $feeRefund->object);
+    }
+
+    public function testStaticUpdateRefund()
+    {
+        $this->mockRequest(
+            'POST',
+            '/v1/application_fees/fee_123/refunds/fr_123',
+            array('metadata' => array('foo' => 'bar')),
+            array('id' => 'fr_123', 'object' => 'fee_refund')
+        );
+
+        $feeRefund = ApplicationFee::updateRefund(
+            'fee_123',
+            'fr_123',
+            array('metadata' => array('foo' => 'bar'))
+        );
+
+        $this->assertSame('fr_123', $feeRefund->id);
+        $this->assertSame('fee_refund', $feeRefund->object);
+    }
+
+    public function testStaticAllRefunds()
+    {
+        $this->mockRequest(
+            'GET',
+            '/v1/application_fees/fee_123/refunds',
+            array(),
+            array('object' => 'list', 'data' => array())
+        );
+
+        $feeRefunds = ApplicationFee::allRefunds(
+            'fee_123'
+        );
+
+        $this->assertSame('list', $feeRefunds->object);
+        $this->assertEmpty($feeRefunds->data);
+    }
 }
