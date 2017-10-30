ab7d957e3f025f94f7a13f14a9da6d8ef380eed5
Brandur <brandur@brandur.org>
2017-10-30 14:09:32 -0700

    Bump version to 5.5.1

--- lib/Stripe.php
+++ lib/Stripe.php
@@ -40,7 +40,7 @@ class Stripe
     //   produce messages.
     public static $logger = null;
 
-    const VERSION = '5.5.0';
+    const VERSION = '5.5.1';
 
     /**
      * @return string The API key used for requests.
