b2d81898ec18e3821cdad2e27d073895e16334ba
Nicolai Cornelis <nickdnk@hotmail.com>
2018-02-07 15:44:03 +0100

    forgot $ prefixes

--- lib/CountrySpec.php
+++ lib/CountrySpec.php
@@ -8,10 +8,10 @@ namespace Stripe;
  * @property string $id
  * @property string $object
  * @property string $default_currency
- * @property mixed supported_bank_account_currencies
- * @property string[] supported_payment_currencies
- * @property string[] supported_payment_methods
- * @property mixed verification_fields
+ * @property mixed $supported_bank_account_currencies
+ * @property string[] $supported_payment_currencies
+ * @property string[] $supported_payment_methods
+ * @property mixed $verification_fields
  *
  * @package Stripe
  */
