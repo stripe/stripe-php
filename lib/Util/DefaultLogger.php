33afd14d74256ec12d7682dd268dd26e9282bf11
Bruce Weirdan <weirdan@gmail.com>
2018-03-01 15:23:47 +0200

    Exception namespace fixes
    Fixed a number of `throw`s that used non-imported exception classes.

--- lib/Util/DefaultLogger.php
+++ lib/Util/DefaultLogger.php
@@ -11,7 +11,7 @@ class DefaultLogger implements LoggerInterface
     public function error($message, array $context = [])
     {
         if (count($context) > 0) {
-            throw new Exception('DefaultLogger does not currently implement context. Please implement if you need it.');
+            throw new \Exception('DefaultLogger does not currently implement context. Please implement if you need it.');
         }
         error_log($message);
     }
