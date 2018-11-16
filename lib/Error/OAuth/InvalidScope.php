37ed9a3ad9407b7694a825068a06ce820923657a
Olivier Bellone <ob@stripe.com>
2018-11-16 10:47:58 +0100

    Update constructors of Stripe exception classes

--- lib/Error/OAuth/InvalidScope.php
+++ lib/Error/OAuth/InvalidScope.php
@@ -4,6 +4,8 @@ namespace Stripe\Error\OAuth;
 
 /**
  * InvalidScope is raised when an invalid scope parameter is provided.
+ *
+ * @package Stripe\Error\OAuth
  */
 class InvalidScope extends OAuthBase
 {
