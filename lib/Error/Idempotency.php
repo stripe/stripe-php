97e131f0987a17afa35ab90423731f3032534136
Olivier Bellone <ob@stripe.com>
2018-11-16 10:47:58 +0100

    Update constructors of Stripe exception classes

--- lib/Error/Idempotency.php
+++ lib/Error/Idempotency.php
@@ -2,6 +2,11 @@
 
 namespace Stripe\Error;
 
+/**
+ * Idempotency is raised in cases where an idempotency key was used improperly.
+ *
+ * @package Stripe\Error
+ */
 class Idempotency extends Base
 {
 }
