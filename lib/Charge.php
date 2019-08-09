9aea300211122404b3714d888cddd28509e4362d
Olivier Bellone <ob@stripe.com>
2019-08-09 11:38:05 -0700

    Remove deprecated methods

--- lib/Charge.php
+++ lib/Charge.php
@@ -121,85 +121,12 @@ class Charge extends ApiResource
      * @param array|null $params
      * @param array|string|null $options
      *
-     * @return Charge The refunded charge.
-     */
-    public function refund($params = null, $options = null)
-    {
-        $url = $this->instanceUrl() . '/refund';
-        list($response, $opts) = $this->_request('post', $url, $params, $options);
-        $this->refreshFrom($response, $opts);
-        return $this;
-    }
-
-    /**
-     * @param array|null $params
-     * @param array|string|null $options
-     *
      * @return Charge The captured charge.
      */
     public function capture($params = null, $options = null)
     {
         $url = $this->instanceUrl() . '/capture';
         list($response, $opts) = $this->_request('post', $url, $params, $options);
-        $this->refreshFrom($response, $opts);
-        return $this;
-    }
-
-    /**
-     * @param array|null $params
-     * @param array|string|null $options
-     *
-     * @deprecated Use the `save` method on the Dispute object
-     *
-     * @return array The updated dispute.
-     */
-    public function updateDispute($params = null, $options = null)
-    {
-        $url = $this->instanceUrl() . '/dispute';
-        list($response, $opts) = $this->_request('post', $url, $params, $options);
-        $this->refreshFrom(['dispute' => $response], $opts, true);
-        return $this->dispute;
-    }
-
-    /**
-     * @param array|string|null $options
-     *
-     * @deprecated Use the `close` method on the Dispute object
-     *
-     * @return Charge The updated charge.
-     */
-    public function closeDispute($options = null)
-    {
-        $url = $this->instanceUrl() . '/dispute/close';
-        list($response, $opts) = $this->_request('post', $url, null, $options);
-        $this->refreshFrom($response, $opts);
-        return $this;
-    }
-
-    /**
-     * @param array|string|null $opts
-     *
-     * @return Charge The updated charge.
-     */
-    public function markAsFraudulent($opts = null)
-    {
-        $params = ['fraud_details' => ['user_report' => 'fraudulent']];
-        $url = $this->instanceUrl();
-        list($response, $opts) = $this->_request('post', $url, $params, $opts);
-        $this->refreshFrom($response, $opts);
-        return $this;
-    }
-
-    /**
-     * @param array|string|null $opts
-     *
-     * @return Charge The updated charge.
-     */
-    public function markAsSafe($opts = null)
-    {
-        $params = ['fraud_details' => ['user_report' => 'safe']];
-        $url = $this->instanceUrl();
-        list($response, $opts) = $this->_request('post', $url, $params, $opts);
         $this->refreshFrom($response, $opts);
         return $this;
     }
