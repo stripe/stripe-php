e2bf05f2235b95de671ef8036378884623bdc44d
Olivier Bellone <ob@stripe.com>
2019-08-09 11:38:05 -0700

    Remove deprecated methods

--- tests/Stripe/SourceTest.php
+++ tests/Stripe/SourceTest.php
@@ -96,7 +96,7 @@ class SourceTest extends TestCase
             'delete',
             '/v1/customers/cus_123/sources/' . $resource->id
         );
-        $resource->delete();
+        $resource->detach();
         $this->assertInstanceOf(\Stripe\Source::class, $resource);
     }
 
