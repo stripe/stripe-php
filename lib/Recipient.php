1ce79aa3c6b7d95f0adedf3be8e22c9feb2768a9
Olivier Bellone <ob@stripe.com>
2019-08-09 11:38:05 -0700

    Remove deprecated methods

--- lib/Recipient.php
+++ lib/Recipient.php
@@ -31,17 +31,4 @@ class Recipient extends ApiResource
     use ApiOperations\Delete;
     use ApiOperations\Retrieve;
     use ApiOperations\Update;
-
-    /**
-     * @param array|null $params
-     *
-     * @return Collection of the Recipient's Transfers
-     */
-    public function transfers($params = null)
-    {
-        $params = $params ?: [];
-        $params['recipient'] = $this->id;
-        $transfers = Transfer::all($params, $this->_opts);
-        return $transfers;
-    }
 }
