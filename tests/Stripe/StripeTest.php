9171c7e7a1906076ef0173c21734e209200bfa90
= <suadhuskic1@gmail.com>
2019-10-15 15:38:47 -0700

    tests

--- tests/Stripe/StripeTest.php
+++ tests/Stripe/StripeTest.php
@@ -27,4 +27,22 @@ class StripeTest extends TestCase
         Stripe::setCABundlePath('path/to/ca/bundle');
         $this->assertEquals('path/to/ca/bundle', Stripe::getCABundlePath());
     }
+
+    public function testInformCallBack()
+    {
+        $test = 0;
+
+        Stripe::setRetryInformCallback(function ($arg) use (&$test) {
+            $test = 3;
+        });
+
+        $this->assertTrue(is_callable(Stripe::getRetryInformCallback()));
+
+        $stripeReflection = new \ReflectionClass(Stripe::class);
+        $informMethod = $stripeReflection->getMethod('informOfRetry');
+        $informMethod->setAccessible(true);
+        $informMethod->invoke(null, 1);
+
+        $this->assertEquals(3, $test);
+    }
 }
