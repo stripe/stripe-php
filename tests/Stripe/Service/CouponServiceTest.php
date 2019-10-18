a73cbe502f9674628da25cf89f90885942c45b6e
Olivier Bellone <ob@stripe.com>
2019-10-18 12:12:23 -0700

    Add AbstractService and a couple of concrete services

---
+++ tests/Stripe/Service/CouponServiceTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Stripe\Service;
+
+/**
+ * @internal
+ */
+final class CouponServiceTest extends \PHPUnit\Framework\TestCase
+{
+    use \Stripe\TestHelper;
+
+    const TEST_RESOURCE_ID = 'COUPON_ID';
+
+    private $client;
+    private $service;
+
+    /**
+     * @before
+     */
+    protected function setUpService()
+    {
+        $this->client = new \Stripe\StripeClient('sk_test_123', null, MOCK_URL);
+        $this->service = new CouponService($this->client);
+    }
+
+    public function testAll()
+    {
+        $this->expectsRequest(
+            'get',
+            '/v1/coupons'
+        );
+        $resources = $this->service->all();
+        static::assertInternalType('array', $resources->data);
+        static::assertInstanceOf(\Stripe\Coupon::class, $resources->data[0]);
+    }
+
+    public function testCreate()
+    {
+        $this->expectsRequest(
+            'post',
+            '/v1/coupons'
+        );
+        $resource = $this->service->create([
+            'percent_off' => 25,
+            'duration' => 'repeating',
+            'duration_in_months' => 3,
+        ]);
+        static::assertInstanceOf(\Stripe\Coupon::class, $resource);
+    }
+
+    public function testDelete()
+    {
+        $this->expectsRequest(
+            'delete',
+            '/v1/coupons/' . self::TEST_RESOURCE_ID
+        );
+        $resource = $this->service->delete(self::TEST_RESOURCE_ID);
+        static::assertInstanceOf(\Stripe\Coupon::class, $resource);
+    }
+
+    public function testRetrieve()
+    {
+        $this->expectsRequest(
+            'get',
+            '/v1/coupons/' . self::TEST_RESOURCE_ID
+        );
+        $resource = $this->service->retrieve(self::TEST_RESOURCE_ID);
+        static::assertInstanceOf(\Stripe\Coupon::class, $resource);
+    }
+
+    public function testUpdate()
+    {
+        $this->expectsRequest(
+            'post',
+            '/v1/coupons/' . self::TEST_RESOURCE_ID
+        );
+        $resource = $this->service->update(self::TEST_RESOURCE_ID, [
+            'metadata' => ['key' => 'value'],
+        ]);
+        static::assertInstanceOf(\Stripe\Coupon::class, $resource);
+    }
+}
