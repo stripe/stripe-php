7b7d08359f6ddf09c140a6616cf4d0ff46e67882
Olivier Bellone <ob@stripe.com>
2019-10-18 12:12:23 -0700

    Add AbstractService and a couple of concrete services

---
+++ tests/Stripe/Service/AbstractServiceTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Stripe\Service;
+
+/**
+ * @internal
+ */
+final class AbstractServiceTest extends \PHPUnit\Framework\TestCase
+{
+    const TEST_RESOURCE_ID = '25OFF';
+
+    private $client;
+    private $service;
+
+    /**
+     * @before
+     */
+    public function setUpMockService()
+    {
+        $this->client = new \Stripe\StripeClient('sk_test_123', null, MOCK_URL);
+        // Testing with CouponService, because testing abstract classes is hard in PHP :/
+        $this->service = new \Stripe\Service\CouponService($this->client);
+    }
+
+    public function testRetrieveThrowsIfIdNullIsNull()
+    {
+        $this->expectException(\Stripe\Exception\InvalidArgumentException::class);
+        $this->expectExceptionMessage('The resource ID cannot be null or whitespace.');
+
+        $this->service->retrieve(null);
+    }
+
+    public function testRetrieveThrowsIfIdNullIsEmpty()
+    {
+        $this->expectException(\Stripe\Exception\InvalidArgumentException::class);
+        $this->expectExceptionMessage('The resource ID cannot be null or whitespace.');
+
+        $this->service->retrieve('');
+    }
+
+    public function testRetrieveThrowsIfIdNullIsWhitespace()
+    {
+        $this->expectException(\Stripe\Exception\InvalidArgumentException::class);
+        $this->expectExceptionMessage('The resource ID cannot be null or whitespace.');
+
+        $this->service->retrieve(' ');
+    }
+}
---
+++ tests/Stripe/Service/PaymentIntentServiceTest.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Stripe\Service;
+
+/**
+ * @internal
+ */
+final class PaymentIntentServiceTest extends \PHPUnit\Framework\TestCase
+{
+    use \Stripe\TestHelper;
+
+    const TEST_RESOURCE_ID = 'pi_123';
+
+    private $client;
+    private $service;
+
+    /**
+     * @before
+     */
+    protected function setUpService()
+    {
+        $this->client = new \Stripe\StripeClient('sk_test_123', null, MOCK_URL);
+        $this->service = new PaymentIntentService($this->client);
+    }
+
+    public function testAll()
+    {
+        $this->expectsRequest(
+            'get',
+            '/v1/payment_intents'
+        );
+        $resources = $this->service->all();
+        static::assertInternalType('array', $resources->data);
+        static::assertInstanceOf(\Stripe\PaymentIntent::class, $resources->data[0]);
+    }
+
+    public function testCapture()
+    {
+        $this->expectsRequest(
+            'post',
+            '/v1/payment_intents/' . self::TEST_RESOURCE_ID . '/capture'
+        );
+        $resource = $this->service->capture(self::TEST_RESOURCE_ID);
+        static::assertInstanceOf(\Stripe\PaymentIntent::class, $resource);
+    }
+
+    public function testConfirm()
+    {
+        $this->expectsRequest(
+            'post',
+            '/v1/payment_intents/' . self::TEST_RESOURCE_ID . '/confirm'
+        );
+        $resource = $this->service->confirm(self::TEST_RESOURCE_ID);
+        static::assertInstanceOf(\Stripe\PaymentIntent::class, $resource);
+    }
+
+    public function testCreate()
+    {
+        $this->expectsRequest(
+            'post',
+            '/v1/payment_intents'
+        );
+        $resource = $this->service->create([
+            'amount' => 100,
+            'currency' => 'usd',
+            'payment_method_types' => ['card'],
+        ]);
+        static::assertInstanceOf(\Stripe\PaymentIntent::class, $resource);
+    }
+
+    public function testRetrieve()
+    {
+        $this->expectsRequest(
+            'get',
+            '/v1/payment_intents/' . self::TEST_RESOURCE_ID
+        );
+        $resource = $this->service->retrieve(self::TEST_RESOURCE_ID);
+        static::assertInstanceOf(\Stripe\PaymentIntent::class, $resource);
+    }
+
+    public function testUpdate()
+    {
+        $this->expectsRequest(
+            'post',
+            '/v1/payment_intents/' . self::TEST_RESOURCE_ID
+        );
+        $resource = $this->service->update(self::TEST_RESOURCE_ID, [
+            'metadata' => ['key' => 'value'],
+        ]);
+        static::assertInstanceOf(\Stripe\PaymentIntent::class, $resource);
+    }
+}
