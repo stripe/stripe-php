0bc7bd4c9e33d0cfb695e8016b29ef72ac82462c
Olivier Bellone <ob@stripe.com>
2019-10-28 20:50:47 -0700

    Add FileService

--- init.php
+++ init.php
@@ -155,4 +155,5 @@ require(dirname(__FILE__) . '/lib/WebhookSignature.php');
 // Services
 require(dirname(__FILE__) . '/lib/Service/AbstractService.php');
 require(dirname(__FILE__) . '/lib/Service/CouponService.php');
+require(dirname(__FILE__) . '/lib/Service/FileService.php');
 require(dirname(__FILE__) . '/lib/Service/PaymentIntentService.php');
