3f6694c4e5898b86957e69f404d866a5e42d2d85
Olivier Bellone <ob@stripe.com>
2019-10-28 18:24:28 -0700

    Add service with methods for nested resources

--- lib/Service/AbstractService.php
+++ lib/Service/AbstractService.php
@@ -45,9 +45,19 @@ abstract class AbstractService
         return $this->request('get', $this->basePath(), $params, $opts);
     }
 
+    protected function allNestedObjects($nestedPath, $parentId, $params, $opts)
+    {
+        return $this->request('get', $this->baseNestedPath($parentId, $nestedPath), $params, $opts);
+    }
+
     protected function createObject($params, $opts)
     {
         return $this->request('post', $this->basePath(), $params, $opts);
+    }
+
+    protected function createNestedObject($nestedPath, $parentId, $params, $opts)
+    {
+        return $this->request('post', $this->baseNestedPath($parentId, $nestedPath), $params, $opts);
     }
 
     protected function deleteObject($id, $params, $opts)
@@ -55,9 +65,19 @@ abstract class AbstractService
         return $this->request('delete', $this->instancePath($id), $params, $opts);
     }
 
+    protected function deleteNestedObject($nestedPath, $parentId, $id, $params, $opts)
+    {
+        return $this->request('delete', $this->instanceNestedPath($parentId, $nestedPath, $id), $params, $opts);
+    }
+
     protected function retrieveObject($id, $params, $opts)
     {
         return $this->request('get', $this->instancePath($id), $params, $opts);
+    }
+
+    protected function retrieveNestedObject($nestedPath, $parentId, $id, $params, $opts)
+    {
+        return $this->request('get', $this->instanceNestedPath($parentId, $nestedPath, $id), $params, $opts);
     }
 
     protected function updateObject($id, $params, $opts)
@@ -65,9 +85,19 @@ abstract class AbstractService
         return $this->request('post', $this->instancePath($id), $params, $opts);
     }
 
+    protected function updateNestedObject($nestedPath, $parentId, $id, $params, $opts)
+    {
+        return $this->request('post', $this->instanceNestedPath($parentId, $nestedPath, $id), $params, $opts);
+    }
+
     protected function request($method, $path, $params, $opts)
     {
         return $this->getClient()->request($method, $path, $params, $opts);
+    }
+
+    protected function baseNestedPath($parentId, $nestedPath)
+    {
+        return $this->instancePath($parentId) . '/' . $nestedPath;
     }
 
     protected function instancePath($id)
@@ -79,5 +109,16 @@ abstract class AbstractService
         }
 
         return $this->basePath() . '/' . \urlencode($id);
+    }
+
+    protected function instanceNestedPath($parentId, $nestedPath, $id)
+    {
+        if (null === $id || '' === \trim($id)) {
+            $msg = 'The resource ID cannot be null or whitespace.';
+
+            throw new \Stripe\Exception\InvalidArgumentException($msg);
+        }
+
+        return $this->baseNestedPath($parentId, $nestedPath) . '/' . \urlencode($id);
     }
 }
