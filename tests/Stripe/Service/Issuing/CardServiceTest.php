2dd42fe08f43ea947a1c2f805b7c2b6686d81c32
Olivier Bellone <ob@stripe.com>
2019-10-28 21:06:25 -0700

    Add namespaced service

---
+++ tests/Stripe/Service/Issuing/CardServiceTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Stripe\Service\Issuing;
+
+/**
+ * @internal
+ */
+final class CardServiceTest extends \PHPUnit\Framework\TestCase
+{
+    use \Stripe\TestHelper;
+
+    const TEST_RESOURCE_ID = 'ic_123';
+
+    private $client;
+    private $service;
+
+    /**
+     * @before
+     */
+    protected function setUpService()
+    {
+        $this->client = new \Stripe\StripeClient('sk_test_123', null, MOCK_URL);
+        $this->service = new CardService($this->client);
+    }
+
+    public function testAll()
+    {
+        $this->expectsRequest(
+            'get',
+            '/v1/issuing/cards'
+        );
+        $resources = $this->service->all();
+        static::assertInternalType('array', $resources->data);
+        static::assertInstanceOf(\Stripe\Issuing\Card::class, $resources->data[0]);
+    }
+
+    public function testCreate()
+    {
+        $this->expectsRequest(
+            'post',
+            '/v1/issuing/cards'
+        );
+        $resource = $this->service->create([
+            'currency' => 'usd',
+            'type' => 'physical',
+        ]);
+        static::assertInstanceOf(\Stripe\Issuing\Card::class, $resource);
+    }
+
+    public function testDetails()
+    {
+        $this->expectsRequest(
+            'get',
+            '/v1/issuing/cards/' . self::TEST_RESOURCE_ID . '/details'
+        );
+        $resource = $this->service->details(self::TEST_RESOURCE_ID);
+        static::assertInstanceOf(\Stripe\Issuing\CardDetails::class, $resource);
+    }
+
+    public function testRetrieve()
+    {
+        $this->expectsRequest(
+            'get',
+            '/v1/issuing/cards/' . self::TEST_RESOURCE_ID
+        );
+        $resource = $this->service->retrieve(self::TEST_RESOURCE_ID);
+        static::assertInstanceOf(\Stripe\Issuing\Card::class, $resource);
+    }
+
+    public function testUpdate()
+    {
+        $this->expectsRequest(
+            'post',
+            '/v1/issuing/cards/' . self::TEST_RESOURCE_ID
+        );
+        $resource = $this->service->update(self::TEST_RESOURCE_ID, [
+            'metadata' => ['key' => 'value'],
+        ]);
+        static::assertInstanceOf(\Stripe\Issuing\Card::class, $resource);
+    }
+}
