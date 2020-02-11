a3f9ac1773155b57a7cd67fef3594f6aa9cb8338
Olivier Bellone <ob@stripe.com>
2020-02-10 17:03:19 -0800

    Codegen for openapi 7562c6b

--- lib/Reporting/ReportRun.php
+++ lib/Reporting/ReportRun.php
@@ -8,13 +8,13 @@ namespace Stripe\Reporting;
  * @property string $id Unique identifier for the object.
  * @property string $object String representing the object's type. Objects of the same type share the same value.
  * @property int $created Time at which the object was created. Measured in seconds since the Unix epoch.
- * @property string|null $error <p>If something should go wrong during the run, a message about the failure (populated when</p><p><code>status=failed</code>).</p>
+ * @property string|null $error If something should go wrong during the run, a message about the failure (populated when <code>status=failed</code>).
  * @property bool $livemode Always <code>true</code>: reports can only be run on live-mode data.
  * @property \Stripe\StripeObject $parameters
  * @property string $report_type The ID of the <a href="https://stripe.com/docs/reporting/statements/api#report-types">report type</a> to run, such as <code>&quot;balance.summary.1&quot;</code>.
- * @property \Stripe\File|null $result <p>The file object representing the result of the report run (populated when</p><p><code>status=succeeded</code>).</p>
- * @property string $status <p>Status of this report run. This will be <code>pending</code> when the run is initially created.</p><p>When the run finishes, this will be set to <code>succeeded</code> and the <code>result</code> field will be populated.</p><p>Rarely, we may encounter an error, at which point this will be set to <code>failed</code> and the <code>error</code> field will be populated.</p>
- * @property int|null $succeeded_at <p>Timestamp at which this run successfully finished (populated when</p><p><code>status=succeeded</code>). Measured in seconds since the Unix epoch.</p>
+ * @property \Stripe\File|null $result The file object representing the result of the report run (populated when <code>status=succeeded</code>).
+ * @property string $status Status of this report run. This will be <code>pending</code> when the run is initially created. When the run finishes, this will be set to <code>succeeded</code> and the <code>result</code> field will be populated. Rarely, we may encounter an error, at which point this will be set to <code>failed</code> and the <code>error</code> field will be populated.
+ * @property int|null $succeeded_at Timestamp at which this run successfully finished (populated when <code>status=succeeded</code>). Measured in seconds since the Unix epoch.
  *
  * @package Stripe\Reporting
  */
