8bee67680dc3074006c42211e891b43959dcd13b
Remi Jannel <remi@stripe.com>
2020-04-22 10:22:06 -0700

    Fix build error on master

--- lib/StripeObject.php
+++ lib/StripeObject.php
@@ -169,7 +169,7 @@ class StripeObject implements \ArrayAccess, \Countable, \JsonSerializable
             return $this->_values[$k];
         }
         if (!empty($this->_transientValues) && $this->_transientValues->includes($k)) {
-            $class = \get_class($this);
+            $class = static::class;
             $attrs = \implode(', ', \array_keys($this->_values));
             $message = "Stripe Notice: Undefined property of {$class} instance: {$k}. "
                     . "HINT: The {$k} attribute was set in the past, however. "
@@ -181,7 +181,7 @@ class StripeObject implements \ArrayAccess, \Countable, \JsonSerializable
 
             return $nullval;
         }
-        $class = \get_class($this);
+        $class = static::class;
         Stripe::getLogger()->error("Stripe Notice: Undefined property of {$class} instance: {$k}");
 
         return $nullval;
@@ -343,14 +343,12 @@ class StripeObject implements \ArrayAccess, \Countable, \JsonSerializable
 
         // a `null` that makes it out of `serializeParamsValue` signals an empty
         // value that we shouldn't appear in the serialized form of the object
-        $updateParams = \array_filter(
+        return \array_filter(
             $updateParams,
             function ($v) {
                 return null !== $v;
             }
         );
-
-        return $updateParams;
     }
 
     public function serializeParamsValue($value, $original, $unsaved, $force, $key = null)
@@ -469,7 +467,7 @@ class StripeObject implements \ArrayAccess, \Countable, \JsonSerializable
 
     public function __toString()
     {
-        $class = \get_class($this);
+        $class = static::class;
 
         return $class . ' JSON: ' . $this->toJSON();
     }
