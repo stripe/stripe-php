20af088b25ea834ce0b1aa1464e2a7339739439f
Remi Jannel <remi@stripe.com>
2020-04-29 10:25:47 -0700

    Fix tests

---
+++ tests/Stripe/PriceTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Stripe;
+
+/**
+ * @internal
+ * @covers \Stripe\Price
+ */
+final class PriceTest extends \PHPUnit\Framework\TestCase
+{
+    use TestHelper;
+
+    const TEST_RESOURCE_ID = 'price_123';
+
+    public function testIsListable()
+    {
+        $this->expectsRequest(
+            'get',
+            '/v1/prices'
+        );
+        $resources = Price::all();
+        static::assertInternalType('array', $resources->data);
+        static::assertInstanceOf(\Stripe\Price::class, $resources->data[0]);
+    }
+
+    public function testIsRetrievable()
+    {
+        $this->expectsRequest(
+            'get',
+            '/v1/prices/' . self::TEST_RESOURCE_ID
+        );
+        $resource = Price::retrieve(self::TEST_RESOURCE_ID);
+        static::assertInstanceOf(\Stripe\Price::class, $resource);
+    }
+
+    public function testIsCreatable()
+    {
+        $this->expectsRequest(
+            'post',
+            '/v1/prices'
+        );
+        $resource = Price::create([
+            'unit_amount' => 2000,
+            'currency' => 'usd',
+            'recurring' => [
+                'interval' => 'month',
+            ],
+            'product_data' => [
+                'name' => 'Product Name',
+            ],
+        ]);
+        static::assertInstanceOf(\Stripe\Price::class, $resource);
+    }
+
+    public function testIsSaveable()
+    {
+        $resource = Price::retrieve(self::TEST_RESOURCE_ID);
+        $resource->metadata['key'] = 'value';
+        $this->expectsRequest(
+            'post',
+            '/v1/prices/' . $resource->id
+        );
+        $resource->save();
+        static::assertInstanceOf(\Stripe\Price::class, $resource);
+    }
+
+    public function testIsUpdatable()
+    {
+        $this->expectsRequest(
+            'post',
+            '/v1/prices/' . self::TEST_RESOURCE_ID
+        );
+        $resource = Price::update(self::TEST_RESOURCE_ID, [
+            'metadata' => ['key' => 'value'],
+        ]);
+        static::assertInstanceOf(\Stripe\Price::class, $resource);
+    }
+}
