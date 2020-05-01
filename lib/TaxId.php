964e9bb6f336b9d9d333f207a59a51e33427813c
Olivier Bellone <ob@stripe.com>
2020-05-01 12:44:27 -0700

    Codegen for openapi db4b68c (#927)

--- lib/TaxId.php
+++ lib/TaxId.php
@@ -16,7 +16,7 @@ namespace Stripe;
  * @property int $created Time at which the object was created. Measured in seconds since the Unix epoch.
  * @property string|\Stripe\Customer $customer ID of the customer.
  * @property bool $livemode Has the value <code>true</code> if the object exists in live mode or the value <code>false</code> if the object exists in test mode.
- * @property string $type Type of the tax ID, one of <code>au_abn</code>, <code>ca_bn</code>, <code>ca_qst</code>, <code>ch_vat</code>, <code>es_cif</code>, <code>eu_vat</code>, <code>hk_br</code>, <code>in_gst</code>, <code>jp_cn</code>, <code>kr_brn</code>, <code>li_uid</code>, <code>mx_rfc</code>, <code>my_itn</code>, <code>my_sst</code>, <code>no_vat</code>, <code>nz_gst</code>, <code>ru_inn</code>, <code>sg_gst</code>, <code>sg_uen</code>, <code>th_vat</code>, <code>tw_vat</code>, <code>us_ein</code>, or <code>za_vat</code>. Note that some legacy tax IDs have type <code>unknown</code>
+ * @property string $type Type of the tax ID, one of <code>au_abn</code>, <code>br_cnpj</code>, <code>br_cpf</code>, <code>ca_bn</code>, <code>ca_qst</code>, <code>ch_vat</code>, <code>es_cif</code>, <code>eu_vat</code>, <code>hk_br</code>, <code>in_gst</code>, <code>jp_cn</code>, <code>kr_brn</code>, <code>li_uid</code>, <code>mx_rfc</code>, <code>my_itn</code>, <code>my_sst</code>, <code>no_vat</code>, <code>nz_gst</code>, <code>ru_inn</code>, <code>sg_gst</code>, <code>sg_uen</code>, <code>th_vat</code>, <code>tw_vat</code>, <code>us_ein</code>, or <code>za_vat</code>. Note that some legacy tax IDs have type <code>unknown</code>
  * @property string $value Value of the tax ID.
  * @property \Stripe\StripeObject $verification
  */
@@ -27,6 +27,8 @@ class TaxId extends ApiResource
     use ApiOperations\Delete;
 
     const TYPE_AU_ABN = 'au_abn';
+    const TYPE_BR_CNPJ = 'br_cnpj';
+    const TYPE_BR_CPF = 'br_cpf';
     const TYPE_CA_BN = 'ca_bn';
     const TYPE_CA_QST = 'ca_qst';
     const TYPE_CH_VAT = 'ch_vat';
