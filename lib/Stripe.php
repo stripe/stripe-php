5df49745eed2d010e28b64831925dbf6fdfc01a4
Richard Marmorstein <richardm@stripe.com>
2020-05-15 14:30:45 -0400

    Bump version to 7.33.1

--- lib/Stripe.php
+++ lib/Stripe.php
@@ -58,7 +58,7 @@ class Stripe
     /** @var float Initial delay between retries, in seconds */
     private static $initialNetworkRetryDelay = 0.5;
 
-    const VERSION = '7.33.0';
+    const VERSION = '7.33.1';
 
     /**
      * @return string the API key used for requests
