6be5424133d65f65084ee6226bc715dfc3fa9c39
Remi Jannel <remi@stripe.com>
2020-07-15 13:57:46 -0700

    Codegen for openapi df0ebe7

--- lib/Checkout/Session.php
+++ lib/Checkout/Session.php
@@ -22,9 +22,12 @@ namespace Stripe\Checkout;
  *
  * @property string $id Unique identifier for the object. Used to pass to <code>redirectToCheckout</code> in Stripe.js.
  * @property string $object String representing the object's type. Objects of the same type share the same value.
+ * @property null|int $amount_subtotal Total of all items before discounts or taxes are applied.
+ * @property null|int $amount_total Total of all items after discounts and taxes are applied.
  * @property null|string $billing_address_collection Describes whether Checkout should collect the customer's billing address.
  * @property string $cancel_url The URL the customer will be directed to if they decide to cancel payment and return to your website.
  * @property null|string $client_reference_id A unique string to reference the Checkout Session. This can be a customer ID, a cart ID, or similar, and can be used to reconcile the session with your internal systems.
+ * @property null|string $currency Three-letter <a href="https://www.iso.org/iso-4217-currency-codes.html">ISO currency code</a>, in lowercase. Must be a <a href="https://stripe.com/docs/currencies">supported currency</a>.
  * @property null|string|\Stripe\Customer $customer The ID of the customer for this session. For Checkout Sessions in <code>payment</code> or <code>subscription</code> mode, Checkout will create a new customer object based on information provided during the session unless an existing customer was provided when the session was created.
  * @property null|string $customer_email If provided, this value will be used when the Customer object is created. If not provided, customers will be asked to enter their email address. Use this parameter to prefill customer data if you already have an email on file. To access information about the customer once a session is complete, use the <code>customer</code> attribute.
  * @property \Stripe\StripeObject[] $display_items The line items, plans, or SKUs purchased by the customer. Prefer using <code>line_items</code>.
@@ -41,6 +44,7 @@ namespace Stripe\Checkout;
  * @property null|string $submit_type Describes the type of transaction being performed by Checkout in order to customize relevant text on the page, such as the submit button. <code>submit_type</code> can only be specified on Checkout Sessions in <code>payment</code> mode, but not Checkout Sessions in <code>subscription</code> or <code>setup</code> mode.
  * @property null|string|\Stripe\Subscription $subscription The ID of the subscription for Checkout Sessions in <code>subscription</code> mode.
  * @property string $success_url The URL the customer will be directed to after the payment or subscription creation is successful.
+ * @property null|\Stripe\StripeObject $total_details Tax and discount details for the computed total amount.
  */
 class Session extends \Stripe\ApiResource
 {
--- lib/TaxRate.php
+++ lib/TaxRate.php
@@ -15,7 +15,7 @@ namespace Stripe;
  *
  * @property string $id Unique identifier for the object.
  * @property string $object String representing the object's type. Objects of the same type share the same value.
- * @property bool $active Defaults to <code>true</code>. When set to <code>false</code>, this tax rate is considered archived and cannot be applied to new applications or Checkout Sessions, but will still be applied to subscriptions and invoices that already have it set.
+ * @property bool $active Defaults to <code>true</code>. When set to <code>false</code>, this tax rate cannot be used with new applications or Checkout Sessions, but will still work for subscriptions and invoices that already have it set.
  * @property int $created Time at which the object was created. Measured in seconds since the Unix epoch.
  * @property null|string $description An arbitrary string attached to the tax rate for your internal use only. It will not be visible to your customers.
  * @property string $display_name The display name of the tax rates as it will appear to your customer on their receipt email, PDF, and the hosted invoice page.
