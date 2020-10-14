081d4ce611a9cff183d053a3863c9b1da3c8b126
Remi Jannel <remi@stripe.com>
2020-10-13 18:06:51 -0700

    Codegen for openapi 4bd4c01

--- lib/Service/InvoiceService.php
+++ lib/Service/InvoiceService.php
@@ -175,8 +175,8 @@ class InvoiceService extends \Stripe\Service\AbstractService
     /**
      * At any time, you can preview the upcoming invoice for a customer. This will show
      * you all the charges that are pending, including subscription renewal charges,
-     * invoice item charges, etc. It will also show you any discount that is applicable
-     * to the customer.
+     * invoice item charges, etc. It will also show you any discounts that are
+     * applicable to the invoice.
      *
      * Note that when you are viewing an upcoming invoice, you are simply viewing a
      * preview – the invoice has not yet been created. As such, the upcoming invoice
