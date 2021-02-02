e070ba8583bb1383f440b71b8081ac7edbaf2ab5
Richard Marmorstein <richardm@stripe.com>
2021-02-02 13:03:57 -0500

    Codegen for openapi 4d877a7

--- lib/Issuing/Dispute.php
+++ lib/Issuing/Dispute.php
@@ -15,10 +15,10 @@ namespace Stripe\Issuing;
  *
  * @property string $id Unique identifier for the object.
  * @property string $object String representing the object's type. Objects of the same type share the same value.
- * @property int $amount Disputed amount. Usually the amount of the <code>disputed_transaction</code>, but can differ (usually because of currency fluctuation).
+ * @property int $amount Disputed amount. Usually the amount of the <code>transaction</code>, but can differ (usually because of currency fluctuation).
  * @property null|\Stripe\BalanceTransaction[] $balance_transactions List of balance transactions associated with the dispute.
  * @property int $created Time at which the object was created. Measured in seconds since the Unix epoch.
- * @property string $currency The currency the <code>disputed_transaction</code> was made in.
+ * @property string $currency The currency the <code>transaction</code> was made in.
  * @property \Stripe\StripeObject $evidence
  * @property bool $livemode Has the value <code>true</code> if the object exists in live mode or the value <code>false</code> if the object exists in test mode.
  * @property \Stripe\StripeObject $metadata Set of <a href="https://stripe.com/docs/api/metadata">key-value pairs</a> that you can attach to an object. This can be useful for storing additional information about the object in a structured format.
