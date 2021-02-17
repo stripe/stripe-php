864c7ccacf3bb6306052864e13b22733209061e5
Richard Marmorstein <richardm@stripe.com>
2021-02-16 19:36:32 -0500

    Codegen for openapi 546b28b

--- lib/PaymentMethod.php
+++ lib/PaymentMethod.php
@@ -18,6 +18,7 @@ namespace Stripe;
  *
  * @property string $id Unique identifier for the object.
  * @property string $object String representing the object's type. Objects of the same type share the same value.
+ * @property \Stripe\StripeObject $afterpay_clearpay
  * @property \Stripe\StripeObject $alipay
  * @property \Stripe\StripeObject $au_becs_debit
  * @property \Stripe\StripeObject $bacs_debit
