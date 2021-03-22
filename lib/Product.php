4f8302abbc601bf722aefa74463832c653c77982
Richard Marmorstein <52928443+richardm-stripe@users.noreply.github.com>
2021-03-22 12:08:55 -0400

    Codegen for openapi f4bfd6d (#1100)

--- lib/Product.php
+++ lib/Product.php
@@ -30,13 +30,13 @@ namespace Stripe;
  * @property bool $livemode Has the value <code>true</code> if the object exists in live mode or the value <code>false</code> if the object exists in test mode.
  * @property \Stripe\StripeObject $metadata Set of <a href="https://stripe.com/docs/api/metadata">key-value pairs</a> that you can attach to an object. This can be useful for storing additional information about the object in a structured format.
  * @property string $name The product's name, meant to be displayable to the customer. Whenever this product is sold via a subscription, name will show up on associated invoice line item descriptions.
- * @property null|\Stripe\StripeObject $package_dimensions The dimensions of this product for shipping purposes. A SKU associated with this product can override this value by having its own <code>package_dimensions</code>. Only applicable to products of <code>type=good</code>.
- * @property null|bool $shippable Whether this product is a shipped good. Only applicable to products of <code>type=good</code>.
+ * @property null|\Stripe\StripeObject $package_dimensions The dimensions of this product for shipping purposes.
+ * @property null|bool $shippable Whether this product is shipped (i.e., physical goods).
  * @property null|string $statement_descriptor Extra information about a product which will appear on your customer's credit card statement. In the case that multiple products are billed at once, the first statement descriptor will be used.
  * @property string $type The type of the product. The product is either of type <code>good</code>, which is eligible for use with Orders and SKUs, or <code>service</code>, which is eligible for use with Subscriptions and Plans.
  * @property null|string $unit_label A label that represents units of this product in Stripe and on customers’ receipts and invoices. When set, this will be included in associated invoice line item descriptions.
  * @property int $updated Time at which the object was last updated. Measured in seconds since the Unix epoch.
- * @property null|string $url A URL of a publicly-accessible webpage for this product. Only applicable to products of <code>type=good</code>.
+ * @property null|string $url A URL of a publicly-accessible webpage for this product.
  */
 class Product extends ApiResource
 {
