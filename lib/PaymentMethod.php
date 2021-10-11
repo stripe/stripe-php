79eacd1ca29482560b6a770118354c967733df47
Richard Marmorstein <richardm@stripe.com>
2021-10-11 08:44:48 -0400

    Codegen for openapi ef061d9

--- lib/PaymentMethod.php
+++ lib/PaymentMethod.php
@@ -36,6 +36,7 @@ namespace Stripe;
  * @property \Stripe\StripeObject $grabpay
  * @property \Stripe\StripeObject $ideal
  * @property \Stripe\StripeObject $interac_present
+ * @property \Stripe\StripeObject $klarna
  * @property bool $livemode Has the value <code>true</code> if the object exists in live mode or the value <code>false</code> if the object exists in test mode.
  * @property null|\Stripe\StripeObject $metadata Set of <a href="https://stripe.com/docs/api/metadata">key-value pairs</a> that you can attach to an object. This can be useful for storing additional information about the object in a structured format.
  * @property \Stripe\StripeObject $oxxo
