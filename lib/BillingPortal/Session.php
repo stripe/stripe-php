caf58192eea0afbeda3e3c4f71c9d73171a23896
Dominic Charley-Roy <dcr@stripe.com>
2021-11-16 19:49:54 -0500

    Codegen for openapi edbb2f0

--- lib/BillingPortal/Session.php
+++ lib/BillingPortal/Session.php
@@ -19,8 +19,6 @@ namespace Stripe\BillingPortal;
  * billing details.
  *
  * Learn more in the <a
- * href="https://stripe.com/docs/billing/subscriptions/customer-portal">product
- * overview</a> and <a
  * href="https://stripe.com/docs/billing/subscriptions/integrating-customer-portal">integration
  * guide</a>.
  *
