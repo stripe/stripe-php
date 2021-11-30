6a33fdd9074c6e456ee303a1b5d7343392849835
Richard Marmorstein <richardm@stripe.com>
2021-11-30 15:51:45 -0500

    Squelch PHPStan

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,15 @@
 
 namespace Stripe;
 
+/**
+ * This class exists so that the test suite is compatible with both
+ * phpunit@5.7 as well as on phpunit@9.0.
+ *
+ * The reason for this is because newer versions of phpunit are
+ * incompatible with PHP<=7.2, but PHP8 is incompatible with phpunit5.6.
+ *
+ * We can remove this class and these methods when we drop support for PHP 7.2 and all earlier versions.
+ */
 class TestCase extends \PHPUnit\Framework\TestCase
 {
     public static function compatAssertIsArray($obj)
@@ -9,35 +18,47 @@ class TestCase extends \PHPUnit\Framework\TestCase
         if (method_exists(static::class, 'assertIsArray')) {
             static::assertIsArray($obj);
         } else {
+            // @phpstan-ignore-next-line
             static::assertInternalType('array', $obj);
         }
     }
-    public function compatExpectExceptionMessageMatches($msg) {
+
+    public function compatExpectExceptionMessageMatches($msg)
+    {
         if (method_exists($this, 'expectExceptionMessageMatches')) {
             $this->expectExceptionMessageMatches($msg);
         } else {
+            // @phpstan-ignore-next-line
             $this->expectExceptionMessageRegExp($msg);
         }
     }
-    public static function compatAssertStringContainsString($a, $b) {
+
+    public static function compatAssertStringContainsString($a, $b)
+    {
         if (method_exists(static::class, 'assertStringContainsString')) {
             static::assertStringContainsString($a, $b);
         } else {
             static::assertContains($a, $b);
         }
     }
-    public function compatAssertIsString($x) {
+
+    public function compatAssertIsString($x)
+    {
         if (method_exists($this, 'assertIsString')) {
-            $this->assertIsString($x);
+            static::assertIsString($x);
         } else {
-            $this->assertInternalType('string', $x);
+            // @phpstan-ignore-next-line
+            static::assertInternalType('string', $x);
         }
     }
-    public static function compatWarningClass() {
+
+    public static function compatWarningClass()
+    {
         if (class_exists('\PHPUnit\Framework\Error\Warning')) {
             return \PHPUnit\Framework\Error\Warning::class;
-        } else {
-            return \PHPUnit_Framework_Error_Warning::class;
         }
+
+        // @phpstan-ignore-next-line
+        return \PHPUnit_Framework_Error_Warning::class;
     }
 }
