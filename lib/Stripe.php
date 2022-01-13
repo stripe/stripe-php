a463289e4f025b49378b9ebec6457d149036c0c7
Dominic Charley-Roy <dcr@stripe.com>
2022-01-13 10:28:18 -0500

    Bump version to 7.110.0

--- lib/Stripe.php
+++ lib/Stripe.php
@@ -58,7 +58,7 @@ class Stripe
     /** @var float Initial delay between retries, in seconds */
     private static $initialNetworkRetryDelay = 0.5;
 
-    const VERSION = '7.109.0';
+    const VERSION = '7.110.0';
 
     /**
      * @return string the API key used for requests
