b663bd7d416b2e794de760d6925c4d2cd7c0c151
Pavel Krymets <pakrym@stripe.com>
2022-03-28 09:50:15 -0700

    test and total_count

--- tests/Stripe/Service/ChargeServiceTest.php
+++ tests/Stripe/Service/ChargeServiceTest.php
@@ -38,6 +38,18 @@ final class ChargeServiceTest extends \Stripe\TestCase
         static::assertInstanceOf(\Stripe\Charge::class, $resources->data[0]);
     }
 
+    public function testSearch()
+    {
+        $this->expectsRequest(
+            'get',
+            '/v1/charges/search',
+        );
+        $resources = $this->service->search(['query' => 'currency:USD']);
+        static::compatAssertIsArray($resources->data);
+        static::assertEquals($resources->total_count, 1);
+        static::assertInstanceOf(\Stripe\Charge::class, $resources->data[0]);
+    }
+
     public function testCapture()
     {
         $this->expectsRequest(
