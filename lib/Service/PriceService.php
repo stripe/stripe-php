3380572fb89dafb49f9692a75aed7f096155ac7b
Pavel Krymets <pakrym@stripe.com>
2022-03-29 12:13:14 -0700

    Codegen for openapi 1700e20

--- lib/Service/PriceService.php
+++ lib/Service/PriceService.php
@@ -55,7 +55,11 @@ class PriceService extends \Stripe\Service\AbstractService
 
     /**
      * Search for prices you’ve previously created using Stripe’s <a
-     * href="/docs/search#search-query-language">Search Query Language</a>.
+     * href="/docs/search#search-query-language">Search Query Language</a>. Don’t use
+     * search in read-after-write flows where strict consistency is necessary. Under
+     * normal operating conditions, data is searchable in less than a minute.
+     * Occasionally, propagation of new or updated data can be up to an hour behind
+     * during outages. Search functionality is not available to merchants in India.
      *
      * @param null|array $params
      * @param null|array|\Stripe\Util\RequestOptions $opts
