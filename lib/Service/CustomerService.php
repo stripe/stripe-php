d406520d0bcce0cab05d97bf00ac0d0a87f76848
Richard Marmorstein <richardm@stripe.com>
2022-04-21 12:31:51 -0700

    Codegen for openapi e07f049

--- lib/Service/CustomerService.php
+++ lib/Service/CustomerService.php
@@ -132,7 +132,7 @@ class CustomerService extends \Stripe\Service\AbstractService
      *
      * @throws \Stripe\Exception\ApiErrorException if the request fails
      *
-     * @return \Stripe\FundingInstructions
+     * @return \Stripe\Customer
      */
     public function createFundingInstructions($id, $params = null, $opts = null)
     {
