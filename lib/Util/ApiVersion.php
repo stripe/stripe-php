87595707a5bd4610d25cb268eef1bfd634b9c368
Pavel Krymets <pakrym@stripe.com>
2022-08-02 13:43:30 -0700

    Codegen for openapi v175

--- lib/Util/ApiVersion.php
+++ lib/Util/ApiVersion.php
@@ -6,5 +6,5 @@ namespace Stripe\Util;
 
 class ApiVersion
 {
-    const CURRENT = '2020-08-27; server_side_confirmation_beta=v1';
+    const CURRENT = '2022-08-01; server_side_confirmation_beta=v1; orders_beta=v4';
 }
