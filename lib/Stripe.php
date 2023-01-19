751d552236543b9509b4179776882dbb0e927fec
Pavel Krymets <pakrym@stripe.com>
2023-01-19 12:27:38 -0800

    Set version to 10.4.0 to simplify merge

--- lib/Stripe.php
+++ lib/Stripe.php
@@ -58,7 +58,7 @@ class Stripe
     /** @var float Initial delay between retries, in seconds */
     private static $initialNetworkRetryDelay = 0.5;
 
-    const VERSION = '10.4.0-beta.3';
+    const VERSION = '10.4.0';
 
     /**
      * @return string the API key used for requests
