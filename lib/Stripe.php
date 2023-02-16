23037558af9d2a8546ebe74873484a9564512919
Annie Li <anniel@stripe.com>
2023-02-16 14:52:20 -0800

    Set version to 10.5.0 to simplify merge

--- lib/Stripe.php
+++ lib/Stripe.php
@@ -58,7 +58,7 @@ class Stripe
     /** @var float Initial delay between retries, in seconds */
     private static $initialNetworkRetryDelay = 0.5;
 
-    const VERSION = '10.6.0-beta.1';
+    const VERSION = '10.5.0';
 
     /**
      * @return string the API key used for requests
