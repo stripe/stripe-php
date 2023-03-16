07af6c8781463795f3e565d5f84ad5ccf4490826
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-03-16 20:08:39 +0000

    Update generated code for v262

--- lib/Tax/Calculation.php
+++ lib/Tax/Calculation.php
@@ -17,6 +17,7 @@ namespace Stripe\Tax;
  * @property null|int $expires_at Timestamp of date at which the tax calculation will expire. Empty if the calculation is an unsaved preview.
  * @property null|\Stripe\Collection<\Stripe\LineItem> $line_items The list of items the customer is purchasing.
  * @property bool $livemode Has the value <code>true</code> if the object exists in live mode or the value <code>false</code> if the object exists in test mode.
+ * @property null|\Stripe\StripeObject $shipping_cost The shipping cost details for the calculation.
  * @property int $tax_amount_exclusive The amount of tax to be collected on top of the line item prices.
  * @property int $tax_amount_inclusive The amount of tax already included in the line item prices.
  * @property int $tax_date Timestamp of date at which the tax rules and rates in effect applies for the calculation.
