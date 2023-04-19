df05eb6278b291cde59822225b43f6055fa8a9c3
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-04-19 09:32:23 +0000

    Update generated code for v302

--- lib/Tax/Registration.php
+++ lib/Tax/Registration.php
@@ -18,12 +18,11 @@ namespace Stripe\Tax;
  * @property string $object String representing the object's type. Objects of the same type share the same value.
  * @property int $active_from Time at which the registration becomes active. Measured in seconds since the Unix epoch.
  * @property string $country Two-letter country code (<a href="https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2">ISO 3166-1 alpha-2</a>).
+ * @property \Stripe\StripeObject $country_options
  * @property int $created Time at which the object was created. Measured in seconds since the Unix epoch.
  * @property null|int $expires_at If set, the registration stops being active at this time. If not set, the registration will be active indefinitely. Measured in seconds since the Unix epoch.
  * @property bool $livemode Has the value <code>true</code> if the object exists in live mode or the value <code>false</code> if the object exists in test mode.
- * @property null|string $state State, county, province, or region.
  * @property string $status The status of the registration. This field is present for convenience and can be deduced from <code>active_from</code> and <code>expires_at</code>.
- * @property string $type The type of the registration. See <a href="https://stripe.com/docs/tax/registering">our guide</a> for more information about registration types.
  */
 class Registration extends \Stripe\ApiResource
 {
@@ -36,11 +35,4 @@ class Registration extends \Stripe\ApiResource
     const STATUS_ACTIVE = 'active';
     const STATUS_EXPIRED = 'expired';
     const STATUS_SCHEDULED = 'scheduled';
-
-    const TYPE_DOMESTIC_SMALL_SELLER = 'domestic_small_seller';
-    const TYPE_IOSS = 'ioss';
-    const TYPE_SIMPLIFIED = 'simplified';
-    const TYPE_STANDARD = 'standard';
-    const TYPE_VAT_OSS_NON_UNION = 'vat_oss_non_union';
-    const TYPE_VAT_OSS_UNION = 'vat_oss_union';
 }
