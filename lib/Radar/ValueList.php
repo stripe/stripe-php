58666a7bd6544671f1222f94762c47636c27dc57
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-08-02 20:06:19 +0000

    Update generated code for v440

--- lib/Radar/ValueList.php
+++ lib/Radar/ValueList.php
@@ -14,7 +14,7 @@ namespace Stripe\Radar;
  * @property string $alias The name of the value list for use in rules.
  * @property int $created Time at which the object was created. Measured in seconds since the Unix epoch.
  * @property string $created_by The name or email address of the user who created this value list.
- * @property string $item_type The type of items in the value list. One of <code>card_fingerprint</code>, <code>card_bin</code>, <code>email</code>, <code>ip_address</code>, <code>country</code>, <code>string</code>, <code>case_sensitive_string</code>, or <code>customer_id</code>.
+ * @property string $item_type The type of items in the value list. One of <code>card_fingerprint</code>, <code>us_bank_account_fingerprint</code>, <code>sepa_debit_fingerprint</code>, <code>card_bin</code>, <code>email</code>, <code>ip_address</code>, <code>country</code>, <code>string</code>, <code>case_sensitive_string</code>, or <code>customer_id</code>.
  * @property \Stripe\Collection<\Stripe\Radar\ValueListItem> $list_items List of items contained within this value list.
  * @property bool $livemode Has the value <code>true</code> if the object exists in live mode or the value <code>false</code> if the object exists in test mode.
  * @property \Stripe\StripeObject $metadata Set of <a href="https://stripe.com/docs/api/metadata">key-value pairs</a> that you can attach to an object. This can be useful for storing additional information about the object in a structured format.
@@ -37,5 +37,7 @@ class ValueList extends \Stripe\ApiResource
     const ITEM_TYPE_CUSTOMER_ID = 'customer_id';
     const ITEM_TYPE_EMAIL = 'email';
     const ITEM_TYPE_IP_ADDRESS = 'ip_address';
+    const ITEM_TYPE_SEPA_DEBIT_FINGERPRINT = 'sepa_debit_fingerprint';
     const ITEM_TYPE_STRING = 'string';
+    const ITEM_TYPE_US_BANK_ACCOUNT_FINGERPRINT = 'us_bank_account_fingerprint';
 }
