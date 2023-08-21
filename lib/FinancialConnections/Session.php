3a4f28bf762c1d9f9259e6be5bf897e4b78cf480
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-08-21 23:04:05 +0000

    Update generated code for v475

--- lib/FinancialConnections/Session.php
+++ lib/FinancialConnections/Session.php
@@ -15,6 +15,7 @@ namespace Stripe\FinancialConnections;
  * @property null|\Stripe\StripeObject $filters
  * @property bool $livemode Has the value <code>true</code> if the object exists in live mode or the value <code>false</code> if the object exists in test mode.
  * @property string[] $permissions Permissions requested for accounts collected during this session.
+ * @property null|string[] $prefetch Data features requested to be retrieved upon account creation.
  * @property null|string $return_url For webview integrations only. Upon completing OAuth login in the native browser, the user will be redirected to this URL to return to your app.
  */
 class Session extends \Stripe\ApiResource
