fbdd15bda6ae394ff0ebfe0eb7a01ee715a7a706
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-09-19 23:52:20 +0000

    Update generated code for v536

--- lib/FileLink.php
+++ lib/FileLink.php
@@ -6,14 +6,14 @@ namespace Stripe;
 
 /**
  * To share the contents of a <code>File</code> object with non-Stripe users, you can
- * create a <code>FileLink</code>. <code>FileLink</code>s contain a URL that can be used to
+ * create a <code>FileLink</code>. <code>FileLink</code>s contain a URL that you can use to
  * retrieve the contents of the file without authentication.
  *
  * @property string $id Unique identifier for the object.
  * @property string $object String representing the object's type. Objects of the same type share the same value.
  * @property int $created Time at which the object was created. Measured in seconds since the Unix epoch.
- * @property bool $expired Whether this link is already expired.
- * @property null|int $expires_at Time at which the link expires.
+ * @property bool $expired Returns if the link is already expired.
+ * @property null|int $expires_at Time that the link expires.
  * @property string|\Stripe\File $file The file object this link points to.
  * @property bool $livemode Has the value <code>true</code> if the object exists in live mode or the value <code>false</code> if the object exists in test mode.
  * @property \Stripe\StripeObject $metadata Set of <a href="https://stripe.com/docs/api/metadata">key-value pairs</a> that you can attach to an object. This can be useful for storing additional information about the object in a structured format.
--- lib/Service/FileService.php
+++ lib/Service/FileService.php
@@ -7,9 +7,9 @@ namespace Stripe\Service;
 class FileService extends \Stripe\Service\AbstractService
 {
     /**
-     * Returns a list of the files that your account has access to. The files are
-     * returned sorted by creation date, with the most recently created files appearing
-     * first.
+     * Returns a list of the files that your account has access to. Stripe sorts and
+     * returns the files by their creation dates, placing the most recently created
+     * files at the top.
      *
      * @param null|array $params
      * @param null|array|\Stripe\Util\RequestOptions $opts
@@ -24,10 +24,9 @@ class FileService extends \Stripe\Service\AbstractService
     }
 
     /**
-     * Retrieves the details of an existing file object. Supply the unique file ID from
-     * a file, and Stripe will return the corresponding file object. To access file
-     * contents, see the <a href="/docs/file-upload#download-file-contents">File Upload
-     * Guide</a>.
+     * Retrieves the details of an existing file object. After you supply a unique file
+     * ID, Stripe returns the corresponding file object. Learn how to <a
+     * href="/docs/file-upload#download-file-contents">access file contents</a>.
      *
      * @param string $id
      * @param null|array $params
