76989ab6dac2c098ef35bdea6e8f0af0fc71d827
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-09-26 18:01:03 +0000

    Update generated code for v554

--- lib/Payout.php
+++ lib/Payout.php
@@ -8,7 +8,7 @@ namespace Stripe;
  * A <code>Payout</code> object is created when you receive funds from Stripe, or when you
  * initiate a payout to either a bank account or debit card of a <a href="/docs/connect/bank-debit-card-payouts">connected
  * Stripe account</a>. You can retrieve individual payouts,
- * as well as list all payouts. Payouts are made on <a href="/docs/connect/manage-payout-schedule">varying
+ * and list all payouts. Payouts are made on <a href="/docs/connect/manage-payout-schedule">varying
  * schedules</a>, depending on your country and
  * industry.
  *
@@ -16,26 +16,26 @@ namespace Stripe;
  *
  * @property string $id Unique identifier for the object.
  * @property string $object String representing the object's type. Objects of the same type share the same value.
- * @property int $amount Amount (in cents (or local equivalent)) to be transferred to your bank account or debit card.
- * @property int $arrival_date Date the payout is expected to arrive in the bank. This factors in delays like weekends or bank holidays.
- * @property bool $automatic Returns <code>true</code> if the payout was created by an <a href="https://stripe.com/docs/payouts#payout-schedule">automated payout schedule</a>, and <code>false</code> if it was <a href="https://stripe.com/docs/payouts#manual-payouts">requested manually</a>.
+ * @property int $amount The amount (in cents (or local equivalent)) that transfers to your bank account or debit card.
+ * @property int $arrival_date Date that you can expect the payout to arrive in the bank. This factors in delays to account for weekends or bank holidays.
+ * @property bool $automatic Returns <code>true</code> if the payout is created by an <a href="https://stripe.com/docs/payouts#payout-schedule">automated payout schedule</a> and <code>false</code> if it's <a href="https://stripe.com/docs/payouts#manual-payouts">requested manually</a>.
  * @property null|string|\Stripe\BalanceTransaction $balance_transaction ID of the balance transaction that describes the impact of this payout on your account balance.
  * @property int $created Time at which the object was created. Measured in seconds since the Unix epoch.
  * @property string $currency Three-letter <a href="https://www.iso.org/iso-4217-currency-codes.html">ISO currency code</a>, in lowercase. Must be a <a href="https://stripe.com/docs/currencies">supported currency</a>.
  * @property null|string $description An arbitrary string attached to the object. Often useful for displaying to users.
- * @property null|string|\Stripe\BankAccount|\Stripe\Card $destination ID of the bank account or card the payout was sent to.
- * @property null|string|\Stripe\BalanceTransaction $failure_balance_transaction If the payout failed or was canceled, this will be the ID of the balance transaction that reversed the initial balance transaction, and puts the funds from the failed payout back in your balance.
- * @property null|string $failure_code Error code explaining reason for payout failure if available. See <a href="https://stripe.com/docs/api#payout_failures">Types of payout failures</a> for a list of failure codes.
- * @property null|string $failure_message Message to user further explaining reason for payout failure if available.
+ * @property null|string|\Stripe\BankAccount|\Stripe\Card $destination ID of the bank account or card the payout is sent to.
+ * @property null|string|\Stripe\BalanceTransaction $failure_balance_transaction If the payout fails or cancels, this is the ID of the balance transaction that reverses the initial balance transaction and returns the funds from the failed payout back in your balance.
+ * @property null|string $failure_code Error code that provides a reason for a payout failure, if available. View our <a href="https://stripe.com/docs/api#payout_failures">list of failure codes</a>.
+ * @property null|string $failure_message Message that provides the reason for a payout failure, if available.
  * @property bool $livemode Has the value <code>true</code> if the object exists in live mode or the value <code>false</code> if the object exists in test mode.
  * @property null|\Stripe\StripeObject $metadata Set of <a href="https://stripe.com/docs/api/metadata">key-value pairs</a> that you can attach to an object. This can be useful for storing additional information about the object in a structured format.
- * @property string $method The method used to send this payout, which can be <code>standard</code> or <code>instant</code>. <code>instant</code> is supported for payouts to debit cards and bank accounts in certain countries. (See <a href="https://stripe.com/docs/payouts/instant-payouts-banks">Bank support for Instant Payouts</a> for more information.)
+ * @property string $method The method used to send this payout, which can be <code>standard</code> or <code>instant</code>. <code>instant</code> is supported for payouts to debit cards and bank accounts in certain countries. Learn more about <a href="https://stripe.com/docs/payouts/instant-payouts-banks">bank support for Instant Payouts</a>.
  * @property null|string|\Stripe\Payout $original_payout If the payout reverses another, this is the ID of the original payout.
- * @property string $reconciliation_status If <code>completed</code>, the <a href="https://stripe.com/docs/api/balance_transactions/list#balance_transaction_list-payout">Balance Transactions API</a> may be used to list all Balance Transactions that were paid out in this payout.
- * @property null|string|\Stripe\Payout $reversed_by If the payout was reversed, this is the ID of the payout that reverses this payout.
- * @property string $source_type The source balance this payout came from. One of <code>card</code>, <code>fpx</code>, or <code>bank_account</code>.
- * @property null|string $statement_descriptor Extra information about a payout to be displayed on the user's bank statement.
- * @property string $status Current status of the payout: <code>paid</code>, <code>pending</code>, <code>in_transit</code>, <code>canceled</code> or <code>failed</code>. A payout is <code>pending</code> until it is submitted to the bank, when it becomes <code>in_transit</code>. The status then changes to <code>paid</code> if the transaction goes through, or to <code>failed</code> or <code>canceled</code> (within 5 business days). Some failed payouts may initially show as <code>paid</code> but then change to <code>failed</code>.
+ * @property string $reconciliation_status If <code>completed</code>, you can use the <a href="https://stripe.com/docs/api/balance_transactions/list#balance_transaction_list-payout">Balance Transactions API</a> to list all balance transactions that are paid out in this payout.
+ * @property null|string|\Stripe\Payout $reversed_by If the payout reverses, this is the ID of the payout that reverses this payout.
+ * @property string $source_type The source balance this payout came from, which can be one of the following: <code>card</code>, <code>fpx</code>, or <code>bank_account</code>.
+ * @property null|string $statement_descriptor Extra information about a payout that displays on the user's bank statement.
+ * @property string $status Current status of the payout: <code>paid</code>, <code>pending</code>, <code>in_transit</code>, <code>canceled</code> or <code>failed</code>. A payout is <code>pending</code> until it's submitted to the bank, when it becomes <code>in_transit</code>. The status changes to <code>paid</code> if the transaction succeeds, or to <code>failed</code> or <code>canceled</code> (within 5 business days). Some payouts that fail might initially show as <code>paid</code>, then change to <code>failed</code>.
  * @property string $type Can be <code>bank_account</code> or <code>card</code>.
  */
 class Payout extends ApiResource
--- lib/Service/RefundService.php
+++ lib/Service/RefundService.php
@@ -44,7 +44,19 @@ class RefundService extends \Stripe\Service\AbstractService
     }
 
     /**
-     * Create a refund.
+     * When you create a new refund, you must specify a Charge or a PaymentIntent
+     * object on which to create it.
+     *
+     * Creating a new refund will refund a charge that has previously been created but
+     * not yet refunded. Funds will be refunded to the credit or debit card that was
+     * originally charged.
+     *
+     * You can optionally refund only part of a charge. You can do so multiple times,
+     * until the entire charge has been refunded.
+     *
+     * Once entirely refunded, a charge can’t be refunded again. This method will raise
+     * an error when called on an already-refunded charge, or when trying to refund
+     * more money than is left on a charge.
      *
      * @param null|array $params
      * @param null|array|\Stripe\Util\RequestOptions $opts
