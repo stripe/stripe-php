438b89f959047bfc4e433c63986f445863f67d78
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-10-10 14:35:09 +0000

    Update generated code for v595

--- lib/Service/SubscriptionService.php
+++ lib/Service/SubscriptionService.php
@@ -155,11 +155,52 @@ class SubscriptionService extends \Stripe\Service\AbstractService
     }
 
     /**
-     * Updates an existing subscription on a customer to match the specified
-     * parameters. When changing plans or quantities, we will optionally prorate the
-     * price we charge next month to make up for any price changes. To preview how the
-     * proration will be calculated, use the <a href="#upcoming_invoice">upcoming
-     * invoice</a> endpoint.
+     * Updates an existing subscription to match the specified parameters. When
+     * changing prices or quantities, we optionally prorate the price we charge next
+     * month to make up for any price changes. To preview how the proration is
+     * calculated, use the <a href="/docs/api/invoices/upcoming">upcoming invoice</a>
+     * endpoint.
+     *
+     * By default, we prorate subscription changes. For example, if a customer signs up
+     * on May 1 for a <currency>100</currency> price, they’ll be billed
+     * <currency>100</currency> immediately. If on May 15 they switch to a
+     * <currency>200</currency> price, then on June 1 they’ll be billed
+     * <currency>250</currency> (<currency>200</currency> for a renewal of her
+     * subscription, plus a <currency>50</currency> prorating adjustment for half of
+     * the previous month’s <currency>100</currency> difference). Similarly, a
+     * downgrade generates a credit that is applied to the next invoice. We also
+     * prorate when you make quantity changes.
+     *
+     * Switching prices does not normally change the billing date or generate an
+     * immediate charge unless:
+     *
+     * <ul> <li>The billing interval is changed (for example, from monthly to
+     * yearly).</li> <li>The subscription moves from free to paid, or paid to
+     * free.</li> <li>A trial starts or ends.</li> </ul>
+     *
+     * In these cases, we apply a credit for the unused time on the previous price,
+     * immediately charge the customer using the new price, and reset the billing date.
+     *
+     * If you want to charge for an upgrade immediately, pass
+     * <code>proration_behavior</code> as <code>always_invoice</code> to create
+     * prorations, automatically invoice the customer for those proration adjustments,
+     * and attempt to collect payment. If you pass <code>create_prorations</code>, the
+     * prorations are created but not automatically invoiced. If you want to bill the
+     * customer for the prorations before the subscription’s renewal date, you need to
+     * manually <a href="/docs/api/invoices/create">invoice the customer</a>.
+     *
+     * If you don’t want to prorate, set the <code>proration_behavior</code> option to
+     * <code>none</code>. With this option, the customer is billed
+     * <currency>100</currency> on May 1 and <currency>200</currency> on June 1.
+     * Similarly, if you set <code>proration_behavior</code> to <code>none</code> when
+     * switching between different billing intervals (for example, from monthly to
+     * yearly), we don’t generate any credits for the old subscription’s unused time.
+     * We still reset the billing date and bill immediately for the new subscription.
+     *
+     * Updating the quantity on a subscription many times in an hour may result in <a
+     * href="/docs/rate-limits">rate limiting</a>. If you need to bill for a frequently
+     * changing quantity, consider integrating <a
+     * href="/docs/billing/subscriptions/usage-based">usage-based billing</a> instead.
      *
      * @param string $id
      * @param null|array $params
