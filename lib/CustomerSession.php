555b047da928a804afe466f2554ec2f9b351abf3
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-11-15 22:52:15 +0000

    Update generated code for v665

--- lib/CustomerSession.php
+++ lib/CustomerSession.php
@@ -10,7 +10,6 @@ namespace Stripe;
  *
  * @property string $object String representing the object's type. Objects of the same type share the same value.
  * @property string $client_secret <p>The client secret of this customer session. Used on the client to set up secure access to the given <code>customer</code>.</p><p>The client secret can be used to provide access to <code>customer</code> from your frontend. It should not be stored, logged, or exposed to anyone other than the relevant customer. Make sure that you have TLS enabled on any page that includes the client secret.</p>
- * @property null|\Stripe\StripeObject $components Configuration for the components supported by this customer session.
  * @property string|\Stripe\Customer $customer The customer the customer session was created for.
  * @property int $expires_at The timestamp at which this customer session will expire.
  * @property bool $livemode Has the value <code>true</code> if the object exists in live mode or the value <code>false</code> if the object exists in test mode.
