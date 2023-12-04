56b0548b259f41b889723d67e193a6938e3890c6
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-12-04 15:50:50 +0000

    Update generated code for v694

--- lib/PaymentLink.php
+++ lib/PaymentLink.php
@@ -26,6 +26,7 @@ namespace Stripe;
  * @property \Stripe\StripeObject[] $custom_fields Collect additional information from your customer using custom fields. Up to 2 fields are supported.
  * @property \Stripe\StripeObject $custom_text
  * @property string $customer_creation Configuration for Customer creation during checkout.
+ * @property null|string $inactive_message The custom message to be displayed to a customer when a payment link is no longer active.
  * @property null|\Stripe\StripeObject $invoice_creation Configuration for creating invoice for payment mode payment links.
  * @property null|\Stripe\Collection<\Stripe\LineItem> $line_items The line items representing what is being sold.
  * @property bool $livemode Has the value <code>true</code> if the object exists in live mode or the value <code>false</code> if the object exists in test mode.
@@ -35,6 +36,7 @@ namespace Stripe;
  * @property string $payment_method_collection Configuration for collecting a payment method during checkout.
  * @property null|string[] $payment_method_types The list of payment method types that customers can use. When <code>null</code>, Stripe will dynamically show relevant payment methods you've enabled in your <a href="https://dashboard.stripe.com/settings/payment_methods">payment method settings</a>.
  * @property \Stripe\StripeObject $phone_number_collection
+ * @property null|\Stripe\StripeObject $restrictions Settings that restrict the usage of a payment link.
  * @property null|\Stripe\StripeObject $shipping_address_collection Configuration for collecting the customer's shipping address.
  * @property \Stripe\StripeObject[] $shipping_options The shipping rate options applied to the session.
  * @property string $submit_type Indicates the type of transaction being performed which customizes relevant text on the page, such as the submit button.
