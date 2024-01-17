72a5da4984888017f41a6eaba6b5c5aac1212a8f
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-01-17 20:49:37 +0000

    Update generated code for v763

--- lib/PaymentMethodConfiguration.php
+++ lib/PaymentMethodConfiguration.php
@@ -37,6 +37,7 @@ namespace Stripe;
  * @property null|\Stripe\StripeObject $card
  * @property null|\Stripe\StripeObject $cartes_bancaires
  * @property null|\Stripe\StripeObject $cashapp
+ * @property null|\Stripe\StripeObject $customer_balance
  * @property null|\Stripe\StripeObject $eps
  * @property null|\Stripe\StripeObject $fpx
  * @property null|\Stripe\StripeObject $giropay
