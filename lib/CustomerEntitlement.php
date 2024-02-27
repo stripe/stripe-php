33263b70b7ddd4ddc384c94bca03f585c33bea7e
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-02-27 13:02:53 +0000

    Update generated code for v848

--- lib/CustomerEntitlement.php
+++ lib/CustomerEntitlement.php
@@ -12,13 +12,8 @@ namespace Stripe;
  * @property string $feature The feature that the customer is entitled to.
  * @property bool $livemode Has the value <code>true</code> if the object exists in live mode or the value <code>false</code> if the object exists in test mode.
  * @property string $lookup_key A unique key you provide as your own system identifier. This may be up to 80 characters.
- * @property null|\Stripe\StripeObject $quantity Contains information about entitlements relating to features with type=quantity. Required when the feature has type=quantity.
- * @property string $type The type of feature.
  */
 class CustomerEntitlement extends ApiResource
 {
     const OBJECT_NAME = 'customer_entitlement';
-
-    const TYPE_QUANTITY = 'quantity';
-    const TYPE_SWITCH = 'switch';
 }
--- lib/Entitlements/Feature.php
+++ lib/Entitlements/Feature.php
@@ -13,8 +13,6 @@ namespace Stripe\Entitlements;
  * @property bool $livemode Has the value <code>true</code> if the object exists in live mode or the value <code>false</code> if the object exists in test mode.
  * @property string $lookup_key A unique key you provide as your own system identifier. This may be up to 80 characters.
  * @property string $name The feature's name, for your own purpose, not meant to be displayable to the customer.
- * @property null|\Stripe\StripeObject $quantity Contains information about type=quantity features. This is required when type=quantity.
- * @property string $type The type of feature.
  */
 class Feature extends \Stripe\ApiResource
 {
@@ -22,7 +20,4 @@ class Feature extends \Stripe\ApiResource
 
     use \Stripe\ApiOperations\All;
     use \Stripe\ApiOperations\Create;
-
-    const TYPE_QUANTITY = 'quantity';
-    const TYPE_SWITCH = 'switch';
 }
