7431076485f2cf67a6db327d3f3ddf8ab7cb8801
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-03-20 20:11:39 +0000

    Update generated code for v887

--- lib/Entitlements/Feature.php
+++ lib/Entitlements/Feature.php
@@ -10,6 +10,7 @@ namespace Stripe\Entitlements;
  *
  * @property string $id Unique identifier for the object.
  * @property string $object String representing the object's type. Objects of the same type share the same value.
+ * @property bool $active Inactive features cannot be attached to new products and will not be returned from the features list endpoint.
  * @property bool $livemode Has the value <code>true</code> if the object exists in live mode or the value <code>false</code> if the object exists in test mode.
  * @property string $lookup_key A unique key you provide as your own system identifier. This may be up to 80 characters.
  * @property string $name The feature's name, for your own purpose, not meant to be displayable to the customer.
