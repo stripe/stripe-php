24d3d9ed8406e10d24452af44597300173655f69
Ramya Rao <ramya@stripe.com>
2024-04-04 15:16:10 -0700

    Bump version to 13.17.0

--- lib/Stripe.php
+++ lib/Stripe.php
@@ -58,7 +58,7 @@ class Stripe
     /** @var float Initial delay between retries, in seconds */
     private static $initialNetworkRetryDelay = 0.5;
 
-    const VERSION = '13.16.0';
+    const VERSION = '13.17.0';
 
     /**
      * @return string the API key used for requests
