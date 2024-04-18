66cc15dec8a190f2543ce77e4003a2200abb0ef0
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-04-18 16:54:37 +0000

    Update generated code for v970

--- lib/Service/Issuing/AuthorizationService.php
+++ lib/Service/Issuing/AuthorizationService.php
@@ -37,6 +37,8 @@ class AuthorizationService extends \Stripe\Service\AbstractService
      * href="/docs/issuing/controls/real-time-authorizations#authorization-handling">respond
      * directly to the webhook request to approve an authorization</a>.
      *
+     * @deprecated  this method is deprecated, please refer to the description for details
+     *
      * @param string $id
      * @param null|array $params
      * @param null|RequestOptionsArray|\Stripe\Util\RequestOptions $opts
@@ -57,6 +59,8 @@ class AuthorizationService extends \Stripe\Service\AbstractService
      * authorization</a> flow. This method is deprecated. Instead, <a
      * href="/docs/issuing/controls/real-time-authorizations#authorization-handling">respond
      * directly to the webhook request to decline an authorization</a>.
+     *
+     * @deprecated  this method is deprecated, please refer to the description for details
      *
      * @param string $id
      * @param null|array $params
