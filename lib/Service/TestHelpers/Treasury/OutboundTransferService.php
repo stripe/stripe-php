2a4afd967e15c375af4de1bd697e5d1d63524228
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-05-06 20:17:59 +0000

    Update generated code for v1011

--- lib/Service/TestHelpers/Treasury/OutboundTransferService.php
+++ lib/Service/TestHelpers/Treasury/OutboundTransferService.php
@@ -65,4 +65,22 @@ class OutboundTransferService extends \Stripe\Service\AbstractService
     {
         return $this->request('post', $this->buildPath('/v1/test_helpers/treasury/outbound_transfers/%s/return', $id), $params, $opts);
     }
+
+    /**
+     * Updates a test mode created OutboundTransfer with tracking details. The
+     * OutboundTransfer must not be cancelable, and cannot be in the
+     * <code>canceled</code> or <code>failed</code> states.
+     *
+     * @param string $id
+     * @param null|array $params
+     * @param null|RequestOptionsArray|\Stripe\Util\RequestOptions $opts
+     *
+     * @throws \Stripe\Exception\ApiErrorException if the request fails
+     *
+     * @return \Stripe\Treasury\OutboundTransfer
+     */
+    public function update($id, $params = null, $opts = null)
+    {
+        return $this->request('post', $this->buildPath('/v1/test_helpers/treasury/outbound_transfers/%s', $id), $params, $opts);
+    }
 }
