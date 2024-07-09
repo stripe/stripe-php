c62bdca1ec66fe4645b36060e2d1765f531ec069
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-07-09 22:19:38 +0000

    Update generated code for v1121

--- lib/Subscription.php
+++ lib/Subscription.php
@@ -149,8 +149,8 @@ class Subscription extends ApiResource
      * Updates an existing subscription to match the specified parameters. When
      * changing prices or quantities, we optionally prorate the price we charge next
      * month to make up for any price changes. To preview how the proration is
-     * calculated, use the <a href="/docs/api/invoices/upcoming">upcoming invoice</a>
-     * endpoint.
+     * calculated, use the <a href="/docs/api/invoices/create_preview">create
+     * preview</a> endpoint.
      *
      * By default, we prorate subscription changes. For example, if a customer signs up
      * on May 1 for a <currency>100</currency> price, they’ll be billed
@@ -166,11 +166,14 @@ class Subscription extends ApiResource
      * immediate charge unless:
      *
      * <ul> <li>The billing interval is changed (for example, from monthly to
-     * yearly).</li> <li>The subscription moves from free to paid, or paid to
-     * free.</li> <li>A trial starts or ends.</li> </ul>
+     * yearly).</li> <li>The subscription moves from free to paid.</li> <li>A trial
+     * starts or ends.</li> </ul>
      *
      * In these cases, we apply a credit for the unused time on the previous price,
      * immediately charge the customer using the new price, and reset the billing date.
+     * Learn about how <a
+     * href="/billing/subscriptions/upgrade-downgrade#immediate-payment">Stripe
+     * immediately attempts payment for subscription changes</a>.
      *
      * If you want to charge for an upgrade immediately, pass
      * <code>proration_behavior</code> as <code>always_invoice</code> to create
