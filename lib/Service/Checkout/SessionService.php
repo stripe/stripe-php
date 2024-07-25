547f7b2f164699e45f8fbdfd24a59bb1c53c9cea
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-07-25 15:10:54 +0000

    Update generated code for v1154

--- lib/Service/Checkout/SessionService.php
+++ lib/Service/Checkout/SessionService.php
@@ -93,4 +93,20 @@ class SessionService extends \Stripe\Service\AbstractService
     {
         return $this->request('get', $this->buildPath('/v1/checkout/sessions/%s', $id), $params, $opts);
     }
+
+    /**
+     * Updates a Session object.
+     *
+     * @param string $id
+     * @param null|array $params
+     * @param null|RequestOptionsArray|\Stripe\Util\RequestOptions $opts
+     *
+     * @throws \Stripe\Exception\ApiErrorException if the request fails
+     *
+     * @return \Stripe\Checkout\Session
+     */
+    public function update($id, $params = null, $opts = null)
+    {
+        return $this->request('post', $this->buildPath('/v1/checkout/sessions/%s', $id), $params, $opts);
+    }
 }
