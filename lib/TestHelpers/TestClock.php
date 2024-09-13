a351187962021af4c5d225d17dd05027107e4be2
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-09-13 19:32:34 +0000

    Update generated code for v1254

--- lib/TestHelpers/TestClock.php
+++ lib/TestHelpers/TestClock.php
@@ -17,7 +17,7 @@ namespace Stripe\TestHelpers;
  * @property bool $livemode Has the value <code>true</code> if the object exists in live mode or the value <code>false</code> if the object exists in test mode.
  * @property null|string $name The custom name supplied at creation.
  * @property string $status The status of the Test Clock.
- * @property \Stripe\StripeObject $status_details
+ * @property null|\Stripe\StripeObject $status_details
  */
 class TestClock extends \Stripe\ApiResource
 {
