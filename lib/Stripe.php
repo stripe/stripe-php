e3f8ec8e13d18bae47143a9f5ffe0265c0a0c73a
Ramya Rao <100975018+ramya-stripe@users.noreply.github.com>
2024-09-27 01:56:52 -0700

    Use the right SDK version

--- lib/Stripe.php
+++ lib/Stripe.php
@@ -64,7 +64,7 @@ class Stripe
     /** @var float Initial delay between retries, in seconds */
     private static $initialNetworkRetryDelay = 0.5;
 
-    const VERSION = '0.8.0';
+    const VERSION = '15.10.0';
 
     /**
      * @return string the API key used for requests
