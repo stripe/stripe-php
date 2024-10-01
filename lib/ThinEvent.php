d845fba347c630e02085b3d6d770b2478ba20798
Prathmesh Ranaut <prathmesh@stripe.com>
2024-10-01 10:16:07 -0400

    Added better description for class

--- lib/ThinEvent.php
+++ lib/ThinEvent.php
@@ -3,6 +3,10 @@
 namespace Stripe;
 
 /**
+ * ThinEvent represents the json that's delivered from an Event Destination.
+ * It's a basic class with no additional methods or properties. Use it to check basic information about a delivered event.
+ * If you want more details, use `$stripeClient->v2->core->events->retrieve(thin_event.id)` to fetch the full event object.
+ *
  * @property string             $id       Unique identifier for the event.
  * @property string             $type     The type of the event.
  * @property string             $created  Time at which the object was created.
