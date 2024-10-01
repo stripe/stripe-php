dd324a26ba67dfc02677681574942adeb7a7cbdd
Prathmesh Ranaut <prathmesh@stripe.com>
2024-10-01 10:01:50 -0400

    Better property descriptions for thin event class

--- lib/ThinEvent.php
+++ lib/ThinEvent.php
@@ -3,12 +3,13 @@
 namespace Stripe;
 
 /**
- * @property string             $id Unique identifier for the event.
- * @property string             $type
- * @property string             $created
- * @property null|string        $context
- * @property null|RelatedObject $related_object
- * @property null|Reason        $reason
+ * @property string             $id       Unique identifier for the event.
+ * @property string             $type     The type of the event.
+ * @property string             $created  Time at which the object was created.
+ * @property null|string        $context  Authentication context needed to fetch the event or related object.
+ * @property null|RelatedObject $related_object // TODO Check if this can be removed and fix the tests
+ * @property null|Reason        $reason   Reason for the event.
+ * @property bool               $livemode Has the value `true` if the object exists in live mode or the value `false` if the object exists in test mode.
  */
 class ThinEvent
 {
