e81186f3c2eeac01f21433b77ee397ad40dbf8e3
jar-stripe <jar@stripe.com>
2025-01-17 08:03:16 -0800

    made v2 event class concrete, and changed convertToStripeObject to use it if we cannot find the identified event subclass (#1805)

--- lib/Util/Util.php
+++ lib/Util/Util.php
@@ -64,7 +64,7 @@ abstract class Util
                     if (\array_key_exists('type', $resp) && \array_key_exists($resp['type'], $eventTypes)) {
                         $class = $eventTypes[$resp['type']];
                     } else {
-                        $class = \Stripe\StripeObject::class;
+                        $class = \Stripe\V2\Event::class;
                     }
                 }
             } elseif (\array_key_exists('data', $resp) && \array_key_exists('next_page_url', $resp)) {
