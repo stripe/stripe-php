064a59cee792c7861ac5d0d99fc0a7cc53a772f5
helenye-stripe <111009531+helenye-stripe@users.noreply.github.com>
2025-03-26 10:14:29 -0700

    Remove tests for endpoints that were removed (#1828)

--- tests/Stripe/Service/SubscriptionItemServiceTest.php
+++ tests/Stripe/Service/SubscriptionItemServiceTest.php
@@ -89,28 +89,4 @@ final class SubscriptionItemServiceTest extends \Stripe\TestCase
         self::assertInstanceOf(\Stripe\SubscriptionItem::class, $resource);
         self::assertTrue($resource->isDeleted());
     }
-
-    public function testCreateUsageRecord()
-    {
-        $this->expectsRequest(
-            'post',
-            '/v1/subscription_items/' . self::TEST_RESOURCE_ID . '/usage_records'
-        );
-        $resource = $this->service->createUsageRecord(self::TEST_RESOURCE_ID, [
-            'quantity' => 100,
-            'timestamp' => 12341234,
-            'action' => 'set',
-        ]);
-    }
-
-    public function testListUsageRecordSummaries()
-    {
-        $this->expectsRequest(
-            'get',
-            '/v1/subscription_items/' . self::TEST_RESOURCE_ID . '/usage_record_summaries'
-        );
-        $resources = $this->service->allUsageRecordSummaries(self::TEST_RESOURCE_ID);
-        self::compatAssertIsArray($resources->data);
-        self::assertInstanceOf(\Stripe\UsageRecordSummary::class, $resources->data[0]);
-    }
 }
