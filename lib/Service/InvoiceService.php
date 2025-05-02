52c2559be5aa9710170f48c3b86ad2d6f2c680f5
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-05-02 20:45:31 +0000

    Update generated code for v1727

--- lib/Service/InvoiceService.php
+++ lib/Service/InvoiceService.php
@@ -120,21 +120,22 @@ class InvoiceService extends AbstractService
      * including subscription renewal charges, invoice item charges, etc. It will also
      * show you any discounts that are applicable to the invoice.
      *
+     * You can also preview the effects of creating or updating a subscription or
+     * subscription schedule, including a preview of any prorations that will take
+     * place. To ensure that the actual proration is calculated exactly the same as the
+     * previewed proration, you should pass the
+     * <code>subscription_details.proration_date</code> parameter when doing the actual
+     * subscription update. The recommended way to get only the prorations being
+     * previewed is to consider only proration line items where
+     * <code>period[start]</code> is equal to the
+     * <code>subscription_details.proration_date</code> value passed in the request.
+     *
      * Note that when you are viewing an upcoming invoice, you are simply viewing a
      * preview – the invoice has not yet been created. As such, the upcoming invoice
      * will not show up in invoice listing calls, and you cannot use the API to pay or
      * edit the invoice. If you want to change the amount that your customer will be
      * billed, you can add, remove, or update pending invoice items, or update the
      * customer’s discount.
-     *
-     * You can preview the effects of updating a subscription, including a preview of
-     * what proration will take place. To ensure that the actual proration is
-     * calculated exactly the same as the previewed proration, you should pass the
-     * <code>subscription_details.proration_date</code> parameter when doing the actual
-     * subscription update. The recommended way to get only the prorations being
-     * previewed is to consider only proration line items where
-     * <code>period[start]</code> is equal to the
-     * <code>subscription_details.proration_date</code> value passed in the request.
      *
      * Note: Currency conversion calculations use the latest exchange rates. Exchange
      * rates may vary between the time of the preview and the time of the actual
