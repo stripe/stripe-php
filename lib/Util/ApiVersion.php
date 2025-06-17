0198a53ea3d164eaf4d5bc53cacd554f0279cc52
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-06-17 15:35:09 +0000

    Update generated code for v1800 and 2025-05-28.preview

--- lib/Util/ApiVersion.php
+++ lib/Util/ApiVersion.php
@@ -6,7 +6,7 @@ namespace Stripe\Util;
 
 class ApiVersion
 {
-    const CURRENT = '2025-05-28.preview';
+    const CURRENT = '2025-06-30.preview';
     const CURRENT_MAJOR = 'preview';
-    const CURRENT_MONTHLY = '2025-05-28';
+    const CURRENT_MONTHLY = '2025-06-30';
 }
