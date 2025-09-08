4fef882393f37197c032828045090805d4d70a3a
David Brownman <xavdid@stripe.com>
2025-09-08 13:03:33 -0700

    hardcode RateLimitException

--- lib/Exception/RateLimitException.php
+++ lib/Exception/RateLimitException.php
@@ -1,7 +1,10 @@
 <?php
 
-// File generated from our OpenAPI spec
-
 namespace Stripe\Exception;
 
-class RateLimitException extends ApiErrorException {}
+/**
+ * RateLimitException is thrown in cases where an account is putting too much
+ * load on Stripe's API servers (usually by performing too many requests).
+ * Please back off on request rate.
+ */
+class RateLimitException extends InvalidRequestException {}
