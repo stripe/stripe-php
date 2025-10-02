9600574600260cd6702472780b76789d3776e29f
David Brownman <xavdid@stripe.com>
2025-10-01 18:40:23 -0700

    Fix comments

--- examples/EventNotificationWebhookHandler.php
+++ examples/EventNotificationWebhookHandler.php
@@ -26,20 +26,20 @@ $app->post('/webhook', static function ($request, $response) use ($client, $webh
 
         // check what type of event notification we have
         if ($event_notification instanceof Stripe\Events\V1BillingMeterErrorReportTriggeredEventNotification) {
-            // there's a relatedObject property you can use for basic logging
+            // there's basic info about the related object in the notification
             echo "Meter with id {$event_notification->related_object->id} reported an error\n";
 
             // or you can fetch the full object form the API for more details
             $meter = $event_notification->fetchRelatedObject();
             echo "Meter {$meter->display_name} ({$meter->id}) had a problem\n";
 
-            # And you can always fetch the full event:
+            // And you can always fetch the full event:
             $event = $event_notification->fetchEvent();
             echo "More info: {$event->data->developer_message_summary}\n";
-        } else if ($event_notification instanceof Stripe\Events\UnknownEventNotification) {
+        } elseif ($event_notification instanceof Stripe\Events\UnknownEventNotification) {
             // this is a valid event type, but it's newer than this SDK, so there's no corresponding class
             // we'll have to match on "type" instead
-            if ($event_notification->type === 'some.new.event') {
+            if ('some.new.event' === $event_notification->type) {
                 // handle it the same way as above
             }
         }
