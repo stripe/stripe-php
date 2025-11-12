18122d2ee85c7d51257f1fa0e7a6bb91cb2490f9
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-11-12 21:53:25 +0000

    Update generated code for v2119 and

---
+++ lib/Issuing/Program.php
@@ -0,0 +1,103 @@
+<?php
+
+// File generated from our OpenAPI spec
+
+namespace Stripe\Issuing;
+
+/**
+ * An Issuing <code>Program</code> represents a card program that the user has access to.
+ *
+ * @property string $id Unique identifier for the object.
+ * @property string $object String representing the object's type. Objects of the same type share the same value.
+ * @property int $created Time at which the object was created. Measured in seconds since the Unix epoch.
+ * @property bool $is_default Whether or not this is the &quot;default&quot; issuing program new cards are created on. Only one active <code>is_default</code> program at the same time.
+ * @property \Stripe\StripeObject $metadata Set of <a href="https://stripe.com/docs/api/metadata">key-value pairs</a> that you can attach to an object. This can be useful for storing additional information about the object in a structured format.
+ * @property null|string $platform_program The platform's Issuing Program for which this program is associated.
+ */
+class Program extends \Stripe\ApiResource
+{
+    const OBJECT_NAME = 'issuing.program';
+
+    use \Stripe\ApiOperations\Update;
+
+    /**
+     * Create a <code>Program</code> object.
+     *
+     * @param null|array{expand?: string[], is_default?: bool, platform_program: string} $params
+     * @param null|array|string $options
+     *
+     * @return Program the created resource
+     *
+     * @throws \Stripe\Exception\ApiErrorException if the request fails
+     */
+    public static function create($params = null, $options = null)
+    {
+        self::_validateParams($params);
+        $url = static::classUrl();
+
+        list($response, $opts) = static::_staticRequest('post', $url, $params, $options);
+        $obj = \Stripe\Util\Util::convertToStripeObject($response->json, $opts);
+        $obj->setLastResponse($response);
+
+        return $obj;
+    }
+
+    /**
+     * List all of the programs the given Issuing user has access to.
+     *
+     * @param null|array{ending_before?: string, expand?: string[], limit?: int, starting_after?: string} $params
+     * @param null|array|string $opts
+     *
+     * @return \Stripe\Collection<Program> of ApiResources
+     *
+     * @throws \Stripe\Exception\ApiErrorException if the request fails
+     */
+    public static function all($params = null, $opts = null)
+    {
+        $url = static::classUrl();
+
+        return static::_requestPage($url, \Stripe\Collection::class, $params, $opts);
+    }
+
+    /**
+     * Retrieves the program specified by the given id.
+     *
+     * @param array|string $id the ID of the API resource to retrieve, or an options array containing an `id` key
+     * @param null|array|string $opts
+     *
+     * @return Program
+     *
+     * @throws \Stripe\Exception\ApiErrorException if the request fails
+     */
+    public static function retrieve($id, $opts = null)
+    {
+        $opts = \Stripe\Util\RequestOptions::parse($opts);
+        $instance = new static($id, $opts);
+        $instance->refresh();
+
+        return $instance;
+    }
+
+    /**
+     * Updates a <code>Program</code> object.
+     *
+     * @param string $id the ID of the resource to update
+     * @param null|array{expand?: string[], is_default?: bool, metadata?: null|array<string, string>} $params
+     * @param null|array|string $opts
+     *
+     * @return Program the updated resource
+     *
+     * @throws \Stripe\Exception\ApiErrorException if the request fails
+     */
+    public static function update($id, $params = null, $opts = null)
+    {
+        self::_validateParams($params);
+        $url = static::resourceUrl($id);
+
+        list($response, $opts) = static::_staticRequest('post', $url, $params, $opts);
+        $obj = \Stripe\Util\Util::convertToStripeObject($response->json, $opts);
+        $obj->setLastResponse($response);
+
+        return $obj;
+    }
+}
