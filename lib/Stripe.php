93853c2d9fa3b41e08fe30b3938ea37cf5859417
Ramya Rao <ramya@stripe.com>
2025-11-24 10:12:14 -0800

    Bump version to 19.1.0-alpha.3

--- lib/Stripe.php
+++ lib/Stripe.php
@@ -64,7 +64,7 @@ class Stripe
     /** @var float Initial delay between retries, in seconds */
     private static $initialNetworkRetryDelay = 0.5;
 
-    const VERSION = '19.1.0-alpha.2';
+    const VERSION = '19.1.0-alpha.3';
 
     /**
      * @return string the API key used for requests
