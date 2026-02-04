8d38dc7a5ca5643f333578e85640cba66eaa0675
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2026-02-04 19:14:27 +0000

    Update generated code for v2165 and

--- lib/Service/FrMealVouchersOnboardingService.php
+++ lib/Service/FrMealVouchersOnboardingService.php
@@ -12,7 +12,9 @@ namespace Stripe\Service;
 class FrMealVouchersOnboardingService extends AbstractService
 {
     /**
-     * Lists French Meal Vouchers Onboarding objects.
+     * Lists <code>French Meal Vouchers Onboarding</code> objects. The objects are
+     * returned in sorted order, with the most recently created objects appearing
+     * first.
      *
      * @param null|array{ending_before?: string, expand?: string[], limit?: int, starting_after?: string} $params
      * @param null|RequestOptionsArray|\Stripe\Util\RequestOptions $opts
@@ -27,8 +29,8 @@ class FrMealVouchersOnboardingService extends AbstractService
     }
 
     /**
-     * Creates a French Meal Vouchers Onboarding object that represents a restaurant’s
-     * onboarding status and starts the onboarding process.
+     * Creates a <code>French Meal Vouchers Onboarding</code> object that represents a
+     * restaurant’s onboarding status and starts the onboarding process.
      *
      * @param null|array{expand?: string[], metadata?: array<string, string>, name: string, postal_code: string, siret: string} $params
      * @param null|RequestOptionsArray|\Stripe\Util\RequestOptions $opts
@@ -43,7 +45,12 @@ class FrMealVouchersOnboardingService extends AbstractService
     }
 
     /**
-     * Retrieves the details of a French Meal Vouchers Onboarding object.
+     * Retrieves the details of a previously created <code>French Meal Vouchers
+     * Onboarding</code> object.
+     *
+     * Supply the unique <code>French Meal Vouchers Onboarding</code> ID that was
+     * returned from your previous request, and Stripe returns the corresponding
+     * onboarding information.
      *
      * @param string $id
      * @param null|array{expand?: string[]} $params
@@ -59,7 +66,13 @@ class FrMealVouchersOnboardingService extends AbstractService
     }
 
     /**
-     * Updates the details of a restaurant’s French Meal Vouchers Onboarding object.
+     * Updates the details of a restaurant’s <code>French Meal Vouchers
+     * Onboarding</code> object by setting the values of the parameters passed. Any
+     * parameters not provided are left unchanged. After you update the object, the
+     * onboarding process automatically restarts.
+     *
+     * You can only update <code>French Meal Vouchers Onboarding</code> objects with
+     * the <code>postal_code</code> field requirement in <code>past_due</code>.
      *
      * @param string $id
      * @param null|array{expand?: string[], postal_code: string} $params
