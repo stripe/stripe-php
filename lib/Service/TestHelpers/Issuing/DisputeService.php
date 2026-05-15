3bca879a0104983f0aec67f83eabf19f6ac07c24
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2026-05-15 22:43:57 +0000

    Update generated code for v2271 and 104722335bd36cf4d60f025dc4b0d7150a7c2200

--- lib/Service/TestHelpers/Issuing/DisputeService.php
+++ lib/Service/TestHelpers/Issuing/DisputeService.php
@@ -12,6 +12,22 @@ namespace Stripe\Service\TestHelpers\Issuing;
 class DisputeService extends \Stripe\Service\AbstractService
 {
     /**
+     * Test helper: closes a test-mode Issuing dispute as won or lost.
+     *
+     * @param string $id
+     * @param null|array{expand?: string[], status: string} $params
+     * @param null|RequestOptionsArray|\Stripe\Util\RequestOptions $opts
+     *
+     * @return \Stripe\Issuing\Dispute
+     *
+     * @throws \Stripe\Exception\ApiErrorException if the request fails
+     */
+    public function close($id, $params = null, $opts = null)
+    {
+        return $this->request('post', $this->buildPath('/v1/test_helpers/issuing/disputes/%s/close', $id), $params, $opts);
+    }
+
+    /**
      * Test helper: populates <code>network_lifecycle.dispute_response</code> on a
      * test-mode Visa Issuing Dispute using placeholder file tokens. Only supported for
      * Visa disputes.
