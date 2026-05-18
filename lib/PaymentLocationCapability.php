11e22990a0288f54f380798ac5ee48a643605ccf
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2026-05-18 18:09:16 +0000

    Update generated code for v2272 and 465cff036983d479dec172c65db53845e8261b31

--- lib/PaymentLocationCapability.php
+++ lib/PaymentLocationCapability.php
@@ -10,6 +10,7 @@ namespace Stripe;
  * @property string $object String representing the object's type. Objects of the same type share the same value.
  * @property string $account The account for which the capability enables functionality.
  * @property string $capability The identifier for the capability.
+ * @property bool $livemode If the object exists in live mode, the value is <code>true</code>. If the object exists in test mode, the value is <code>false</code>.
  * @property string $location The payment location for which the capability enables functionality.
  * @property bool $requested Whether the capability has been requested.
  * @property null|int $requested_at Time at which the capability was requested. Measured in seconds since the Unix epoch.
@@ -26,6 +27,43 @@ class PaymentLocationCapability extends ApiResource
     const STATUS_INACTIVE = 'inactive';
     const STATUS_PENDING = 'pending';
     const STATUS_UNREQUESTED = 'unrequested';
+
+    /**
+     * Returns a list of <code>PaymentLocationCapability</code> objects associated with
+     * the location.
+     *
+     * @param null|array{expand?: string[], location: string} $params
+     * @param null|array|string $opts
+     *
+     * @return Collection<PaymentLocationCapability> of ApiResources
+     *
+     * @throws Exception\ApiErrorException if the request fails
+     */
+    public static function all($params = null, $opts = null)
+    {
+        $url = static::classUrl();
+
+        return static::_requestPage($url, Collection::class, $params, $opts);
+    }
+
+    /**
+     * Retrieves information about the specified Payment Location Capability.
+     *
+     * @param array|string $id the ID of the API resource to retrieve, or an options array containing an `id` key
+     * @param null|array|string $opts
+     *
+     * @return PaymentLocationCapability
+     *
+     * @throws Exception\ApiErrorException if the request fails
+     */
+    public static function retrieve($id, $opts = null)
+    {
+        $opts = Util\RequestOptions::parse($opts);
+        $instance = new static($id, $opts);
+        $instance->refresh();
+
+        return $instance;
+    }
 
     /**
      * Updates a specified Payment Location Capability. Request or remove a payment
--- lib/Service/PaymentLocationCapabilityService.php
+++ lib/Service/PaymentLocationCapabilityService.php
@@ -12,6 +12,38 @@ namespace Stripe\Service;
 class PaymentLocationCapabilityService extends AbstractService
 {
     /**
+     * Returns a list of <code>PaymentLocationCapability</code> objects associated with
+     * the location.
+     *
+     * @param null|array{expand?: string[], location: string} $params
+     * @param null|RequestOptionsArray|\Stripe\Util\RequestOptions $opts
+     *
+     * @return \Stripe\Collection<\Stripe\PaymentLocationCapability>
+     *
+     * @throws \Stripe\Exception\ApiErrorException if the request fails
+     */
+    public function all($params = null, $opts = null)
+    {
+        return $this->requestCollection('get', '/v1/payment_location_capabilities', $params, $opts);
+    }
+
+    /**
+     * Retrieves information about the specified Payment Location Capability.
+     *
+     * @param string $id
+     * @param null|array{expand?: string[], location: string} $params
+     * @param null|RequestOptionsArray|\Stripe\Util\RequestOptions $opts
+     *
+     * @return \Stripe\PaymentLocationCapability
+     *
+     * @throws \Stripe\Exception\ApiErrorException if the request fails
+     */
+    public function retrieve($id, $params = null, $opts = null)
+    {
+        return $this->request('get', $this->buildPath('/v1/payment_location_capabilities/%s', $id), $params, $opts);
+    }
+
+    /**
      * Updates a specified Payment Location Capability. Request or remove a payment
      * location capability by updating its <code>requested</code> parameter.
      *
