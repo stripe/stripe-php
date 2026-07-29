2a9dd6bcae7a6714833fcf9f05551345324fbcfc
Jesse Rosalia <jar@stripe.com>
2026-07-29 16:37:41 -0700

    Bump version to 21.2.0-beta.1

--- lib/Stripe.php
+++ lib/Stripe.php
@@ -61,7 +61,7 @@ class Stripe
     /** @var float Initial delay between retries, in seconds */
     private static $initialNetworkRetryDelay = 0.5;
 
-    const VERSION = '20.4.0-beta.1';
+    const VERSION = '21.2.0-beta.1';
 
     /**
      * @return string the API key used for requests
