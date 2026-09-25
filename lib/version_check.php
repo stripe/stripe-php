33b03474a2a7c0ceede0de4d9f365655445c4f93
Stripe SDK <noreply@github.com>
2026-09-25 19:50:42 +0000

    ⚠️ Drop support for PHP 7.2 and 7.3
    Committed-By-Agent: goose
    Orbit-Session-Id: 064fdd2c-3091-4e62-acfb-bd223d84afc9

--- lib/version_check.php
+++ lib/version_check.php
@@ -2,8 +2,8 @@
 
 // see: https://docs.stripe.com/sdks/versioning?lang=php
 
-if (\PHP_VERSION_ID < 70200) {
+if (\PHP_VERSION_ID < 70400) {
     throw new RuntimeException(
-        'The Stripe SDK requires PHP 7.2.0 or later. You are running PHP ' . \PHP_VERSION . '.'
+        'The Stripe SDK requires PHP 7.4.0 or later. You are running PHP ' . \PHP_VERSION . '.'
     );
 }
